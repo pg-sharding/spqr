02ab74d482fe25afe4c4f95818e1d0591d9c79c4
denchick <denchick@yandex-team.ru>
2021-08-20 19:14:09 +0500

    mkdir demo

--- demo/bin/entrypoint.sh
+++ demo/bin/entrypoint.sh
@@ -3,8 +3,6 @@ set -ex
 
 rm -fr /var/lib/postgresql/13/main/ 
 sudo -u postgres /usr/lib/postgresql/13/bin/pg_ctl -D /var/lib/postgresql/13/main/ init
-
 setup
-
 sudo -u postgres /usr/lib/postgresql/13/bin/postgres -D /var/lib/postgresql/13/main
 
