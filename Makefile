045a440b14bca2aa519e26fe33801d7a9ffb3374
reshke <Kirill Reshke>
2021-08-31 22:54:53 +0500

    Simple test for router

--- Makefile
+++ Makefile
@@ -9,5 +9,8 @@ init:
 	 go mod download
 	 go mod vendor
 
+test:
+	docker-compose up  --remove-orphans --exit-code-from client --build router shard1 shard2 client
+
 yaccgen:
 	goyacc -o yacc/spqrparser/sql.go -p yy yacc/spqrparser/sql.y
---
+++ docker/tests/bin/simple.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sleep 100
+
+psql "host=spqr_router_1_1 sslmode=disable user=user1 dbname=db1 port=6432" -c 'insert into x values(1)' || {
+	echo "ERROR: tests failed"
+	exit 1
+}
+
+psql "host=spqr_router_1_1 sslmode=disable user=user1 dbname=db1 port=6432" -c 'insert into x values(11)' || {
+	echo "ERROR: tests failed"
+	exit 1
+}
+
+psql "host=spqr_router_1_1 sslmode=disable user=user1 dbname=db1 port=6432" -c 'select from x where w_id = 11' || {
+	echo "ERROR: tests failed"
+	exit 1
+}
+
+psql "host=spqr_router_1_1 sslmode=disable user=user1 dbname=db1 port=6432" -c 'select from x where w_id = 1' || {
+	echo "ERROR: tests failed"
+	exit 1
+}
