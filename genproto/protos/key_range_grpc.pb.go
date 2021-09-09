fb2c42b9dba3895e886e2a2f12523d36a44a3bdc
reshke <reshkekirill@gmail.com>
2021-09-09 12:11:33 +0500

    Key range api basics

--- genproto/protos/key_range_grpc.pb.go
+++ genproto/protos/key_range_grpc.pb.go
@@ -18,7 +18,10 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type KeyRangeServiceClient interface {
-	ListKeyRange(ctx context.Context, in *KeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error)
+	ListKeyRange(ctx context.Context, in *ListKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error)
+	LockKeyRange(ctx context.Context, in *LockKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error)
+	UnlockKeyRange(ctx context.Context, in *UnlockKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error)
+	SplitKeyRange(ctx context.Context, in *SplitKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error)
 }
 
 type keyRangeServiceClient struct {
@@ -29,9 +32,36 @@ func NewKeyRangeServiceClient(cc grpc.ClientConnInterface) KeyRangeServiceClient
 	return &keyRangeServiceClient{cc}
 }
 
-func (c *keyRangeServiceClient) ListKeyRange(ctx context.Context, in *KeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error) {
+func (c *keyRangeServiceClient) ListKeyRange(ctx context.Context, in *ListKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error) {
 	out := new(KeyRangeReply)
 	err := c.cc.Invoke(ctx, "/yandex.spqr.KeyRangeService/ListKeyRange", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *keyRangeServiceClient) LockKeyRange(ctx context.Context, in *LockKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error) {
+	out := new(KeyRangeReply)
+	err := c.cc.Invoke(ctx, "/yandex.spqr.KeyRangeService/LockKeyRange", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *keyRangeServiceClient) UnlockKeyRange(ctx context.Context, in *UnlockKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error) {
+	out := new(KeyRangeReply)
+	err := c.cc.Invoke(ctx, "/yandex.spqr.KeyRangeService/UnlockKeyRange", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *keyRangeServiceClient) SplitKeyRange(ctx context.Context, in *SplitKeyRangeRequest, opts ...grpc.CallOption) (*KeyRangeReply, error) {
+	out := new(KeyRangeReply)
+	err := c.cc.Invoke(ctx, "/yandex.spqr.KeyRangeService/SplitKeyRange", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +72,10 @@ func (c *keyRangeServiceClient) ListKeyRange(ctx context.Context, in *KeyRangeRe
 // All implementations must embed UnimplementedKeyRangeServiceServer
 // for forward compatibility
 type KeyRangeServiceServer interface {
-	ListKeyRange(context.Context, *KeyRangeRequest) (*KeyRangeReply, error)
+	ListKeyRange(context.Context, *ListKeyRangeRequest) (*KeyRangeReply, error)
+	LockKeyRange(context.Context, *LockKeyRangeRequest) (*KeyRangeReply, error)
+	UnlockKeyRange(context.Context, *UnlockKeyRangeRequest) (*KeyRangeReply, error)
+	SplitKeyRange(context.Context, *SplitKeyRangeRequest) (*KeyRangeReply, error)
 	mustEmbedUnimplementedKeyRangeServiceServer()
 }
 
@@ -50,8 +83,17 @@ type KeyRangeServiceServer interface {
 type UnimplementedKeyRangeServiceServer struct {
 }
 
-func (UnimplementedKeyRangeServiceServer) ListKeyRange(context.Context, *KeyRangeRequest) (*KeyRangeReply, error) {
+func (UnimplementedKeyRangeServiceServer) ListKeyRange(context.Context, *ListKeyRangeRequest) (*KeyRangeReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListKeyRange not implemented")
+}
+func (UnimplementedKeyRangeServiceServer) LockKeyRange(context.Context, *LockKeyRangeRequest) (*KeyRangeReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method LockKeyRange not implemented")
+}
+func (UnimplementedKeyRangeServiceServer) UnlockKeyRange(context.Context, *UnlockKeyRangeRequest) (*KeyRangeReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UnlockKeyRange not implemented")
+}
+func (UnimplementedKeyRangeServiceServer) SplitKeyRange(context.Context, *SplitKeyRangeRequest) (*KeyRangeReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SplitKeyRange not implemented")
 }
 func (UnimplementedKeyRangeServiceServer) mustEmbedUnimplementedKeyRangeServiceServer() {}
 
@@ -67,7 +109,7 @@ func RegisterKeyRangeServiceServer(s grpc.ServiceRegistrar, srv KeyRangeServiceS
 }
 
 func _KeyRangeService_ListKeyRange_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(KeyRangeRequest)
+	in := new(ListKeyRangeRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -79,7 +121,61 @@ func _KeyRangeService_ListKeyRange_Handler(srv interface{}, ctx context.Context,
 		FullMethod: "/yandex.spqr.KeyRangeService/ListKeyRange",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(KeyRangeServiceServer).ListKeyRange(ctx, req.(*KeyRangeRequest))
+		return srv.(KeyRangeServiceServer).ListKeyRange(ctx, req.(*ListKeyRangeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _KeyRangeService_LockKeyRange_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(LockKeyRangeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(KeyRangeServiceServer).LockKeyRange(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/yandex.spqr.KeyRangeService/LockKeyRange",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(KeyRangeServiceServer).LockKeyRange(ctx, req.(*LockKeyRangeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _KeyRangeService_UnlockKeyRange_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UnlockKeyRangeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(KeyRangeServiceServer).UnlockKeyRange(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/yandex.spqr.KeyRangeService/UnlockKeyRange",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(KeyRangeServiceServer).UnlockKeyRange(ctx, req.(*UnlockKeyRangeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _KeyRangeService_SplitKeyRange_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SplitKeyRangeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(KeyRangeServiceServer).SplitKeyRange(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/yandex.spqr.KeyRangeService/SplitKeyRange",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(KeyRangeServiceServer).SplitKeyRange(ctx, req.(*SplitKeyRangeRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -94,6 +190,18 @@ var KeyRangeService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ListKeyRange",
 			Handler:    _KeyRangeService_ListKeyRange_Handler,
+		},
+		{
+			MethodName: "LockKeyRange",
+			Handler:    _KeyRangeService_LockKeyRange_Handler,
+		},
+		{
+			MethodName: "UnlockKeyRange",
+			Handler:    _KeyRangeService_UnlockKeyRange_Handler,
+		},
+		{
+			MethodName: "SplitKeyRange",
+			Handler:    _KeyRangeService_SplitKeyRange_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
