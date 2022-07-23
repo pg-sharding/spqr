f104fdc5c13b66edd4a791c6fddf1c9aa46cc758
reshke <reshke@double.cloud>
2022-07-23 16:48:33 +0000

    Fix buf with values-based insert statement routing

--- test/regress/sql/shard_routing.sql
+++ test/regress/sql/shard_routing.sql
@@ -13,6 +13,30 @@ SELECT * FROM xx WHERE w_id >= 1;
 SELECT * FROM xx WHERE w_id >= 20;
 SELECT * FROM xx WHERE w_id >= 21;
 
+INSERT INTO xxtt1 (w_id) VALUES(1);
+INSERT INTO xxtt1 (w_id) VALUES(15);
+INSERT INTO xxtt1 (w_id) VALUES(21);
+
+INSERT INTO xxtt1 (i, w_id) VALUES(1, 1);
+INSERT INTO xxtt1 (i, w_id) VALUES(15, 15);
+INSERT INTO xxtt1 (i, w_id) VALUES(21, 21);
+
+INSERT INTO xxtt1 (w_id, i) VALUES(1, 1);
+INSERT INTO xxtt1 (w_id, i) VALUES(15, -12);
+INSERT INTO xxtt1 (w_id, i) VALUES(21, 12);
+
+INSERT INTO xxtt1 (i, w_id) VALUES(1, 1);
+INSERT INTO xxtt1 (i, w_id) VALUES(-12, 15);
+INSERT INTO xxtt1 (i, w_id) VALUES(2121221, 21);
+
+INSERT INTO xxtt1 (i, j, w_id) VALUES(-12, 1, 1);
+INSERT INTO xxtt1 (i, w_id, j) VALUES(-12, 15, 123123);
+INSERT INTO xxtt1 (j, i, w_id) VALUES(2121221, -211212, 23);
+
+SELECT * FROM xxtt1 WHERE w_id >= 1;
+SELECT * FROM xxtt1 WHERE w_id >= 20;
+SELECT * FROM xxtt1 WHERE w_id >= 21;
+
 DROP TABLE xx;
 DROP TABLE xxtt1;
 
