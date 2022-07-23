3c0ce74cf47c802d92aab47ce016521a79ca7181
reshke <reshke@double.cloud>
2022-07-23 15:50:10 +0000

    Implement check makefile target to run regression tests

--- test/regress/run_tests.sh
+++ test/regress/run_tests.sh
@@ -6,13 +6,16 @@ spqr_pid=$(echo $!)
 # XXX:bootstrap database
 #psql ""
 
+export PGOPTIONS='-c spqr_reply_shard_match=on'
+
 # wait for spqr to start
 sleep 2
 
-
+echo '' > regression.diffs
 for line in `cat schedule`
 do
 	psql "host=localhost port=6432 dbname=regression_spqr sslmode=disable" -f ./sql/$line.sql > ./results/$line.out 2>&1
+	diff ./expected/$line.out ./results/$line.out >> regression.diffs
 done
 
 kill $spqr_pid
