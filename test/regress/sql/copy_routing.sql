b187800668198c74014d339b0f30786246ea3023
kirill reshke <reshke@double.cloud>
2022-07-29 15:09:42 +0500

    Add test to copy feature wuth proxy router (#58)
    * Add test to copy feature wuth proxy router
    
    * Copy out processing basic

---
+++ test/regress/sql/copy_routing.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS xxcopy;
+CREATE TABLE xxcopy (id int);
+
+COPY xxcopy FROM STDIN WHERE id <= 10;
+1
+2
+3
+4
+5
+12
+3434
+43
+\.
+
+SELECT * FROM xxcopy WHERE id <= 10;
+
+COPY xxcopy FROM STDIN WHERE id <= 30;
+1
+2
+3
+4
+5
+12
+23
+22
+32
+42
+3434
+43
+\.
+
+SELECT * FROM xxcopy WHERE id <= 30;
