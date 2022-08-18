cc3cedc5b5a48a1eacd7c4bbe96b0adb8faaaaf7
Yury Frolov <yura200253@gmail.com>
2022-08-18 13:40:11 +0500

    Commented router + coordinator test

--- test/regress/run_tests.sh
+++ test/regress/run_tests.sh
@@ -1,7 +1,7 @@
 #set -x
 
 LOGFILE=router.log
-COORDINATOR_LOGFILE=coordinator.log
+#COORDINATOR_LOGFILE=coordinator.log
 
 echo 'Router tests:' >> LOGFILE
 
@@ -44,43 +44,43 @@ fi
 ## coordinator + router setup tests
 ## TODO: start qdb
 
-echo 'Coordinator + router tests:' >> $LOGFILE
-
-../../spqr-rr run -c ./conf/regression-cc.yaml > $LOGFILE 2>&1 &
-
-spqr_pid=$!
-
-../../spqr-coordinator run -c ./conf/regression-coord.yaml > $COORDINATOR_LOGFILE 2>&1 &
-
-spqr_coordinator_pid=$!
-
-export PGOPTIONS='-c spqr_reply_shard_match=on'
-
-# wait for spqr to start
-sleep 2
-
-rm -f regression.diffs
-touch regression.diffs
-
-
-while IFS= read -r line
-do
-  psql "host=localhost port=7002 dbname=regression_spqr sslmode=disable" -f ./adm_sql/"$line".sql > ./results/"$line".out 2>&1
-  diff ./expected/"$line".out ./results/"$line".out >> regression.diffs
-done < adm_schedule
-
-kill "$spqr_pid"
-kill "$spqr_coordinator_pid"
-
-
-if [ -s regression.diffs ]; then
-	echo '
-
-	Tests exp/actual diffs:
-
-	'
-	cat regression.diffs
-
-	exit 2
-fi
+#echo 'Coordinator + router tests:' >> $LOGFILE
+#
+#../../spqr-rr run -c ./conf/regression-cc.yaml > $LOGFILE 2>&1 &
+#
+#spqr_pid=$!
+#
+#../../spqr-coordinator run -c ./conf/regression-coord.yaml > $COORDINATOR_LOGFILE 2>&1 &
+#
+#spqr_coordinator_pid=$!
+#
+#export PGOPTIONS='-c spqr_reply_shard_match=on'
+#
+## wait for spqr to start
+#sleep 2
+#
+#rm -f regression.diffs
+#touch regression.diffs
+#
+#
+#while IFS= read -r line
+#do
+#  psql "host=localhost port=7002 dbname=regression_spqr sslmode=disable" -f ./adm_sql/"$line".sql > ./results/"$line".out 2>&1
+#  diff ./expected/"$line".out ./results/"$line".out >> regression.diffs
+#done < adm_schedule
+#
+#kill "$spqr_pid"
+#kill "$spqr_coordinator_pid"
+#
+#
+#if [ -s regression.diffs ]; then
+#	echo '
+#
+#	Tests exp/actual diffs:
+#
+#	'
+#	cat regression.diffs
+#
+#	exit 2
+#fi
 
