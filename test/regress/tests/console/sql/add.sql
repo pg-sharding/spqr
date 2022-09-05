583d9d0e436015bf3e53718a9c2942bff30d4c1b
denchick <denchick@yandex-team.ru>
2022-09-05 11:30:15 +0500

    fix tests

--- test/regress/tests/console/sql/add.sql
+++ test/regress/tests/console/sql/add.sql
@@ -7,7 +7,6 @@ SHOW key_ranges;
 SHOW sharding_rules;
 DROP SHARDING RULE rule1;
 DROP KEY RANGE krid1;
-ADD KEY RANGE krid1 FROM 1 TO 10 ROUTE TO sh1;
 ADD KEY RANGE krid2 FROM 11 TO 20 ROUTE TO sh2;
 ADD SHARDING RULE rule1 COLUMNS id;
 SHOW key_ranges;
