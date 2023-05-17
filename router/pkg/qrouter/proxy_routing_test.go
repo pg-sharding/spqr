8288e8a21f2b13fde50fa86b199433b79eac5a84
reshke <reshke@double.cloud>
2023-05-17 06:49:40 +0000

    Add unit testsX

---
+++ router/pkg/qrouter/proxy_routing_test.go
@@ -0,0 +1,83 @@
+package qrouter
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseComment(t *testing.T) {
+	assert := assert.New(t)
+
+	type tmp struct {
+		sample string
+		exp    map[string]string
+		err    error
+	}
+
+	for _, tt := range []tmp{
+		{
+			sample: "lol: kek",
+			exp: map[string]string{
+				"lol": "kek",
+			},
+			err: nil,
+		},
+
+		{
+			sample: "lol kek",
+			err:    fmt.Errorf("no colon"),
+		},
+
+		{
+			sample: "lol: kek lol2: kek2",
+			err:    fmt.Errorf("no comma"),
+		},
+
+		{
+			sample: "vguoyguoygoyy",
+			err:    fmt.Errorf("wtf"),
+		},
+
+		{
+			sample: "lol: kek, lol2 : kek2",
+			exp: map[string]string{
+				"lol":  "kek",
+				"lol2": "kek2",
+			},
+			err: nil,
+		},
+		{
+			sample: "lol: kek , lol2 : kek2   , lol3:     kek3",
+			exp: map[string]string{
+				"lol":  "kek",
+				"lol2": "kek2",
+				"lol3": "kek3",
+			},
+			err: nil,
+		},
+		{
+			sample: "lol: kek, lol2 : kek2,   lol3:     kek3    , lol4:kek4   ,  lol5 :kek5",
+			exp: map[string]string{
+				"lol":  "kek",
+				"lol2": "kek2",
+				"lol3": "kek3",
+				"lol4": "kek4",
+				"lol5": "kek5",
+			},
+			err: nil,
+		},
+	} {
+
+		mp, err := parseComment(tt.sample)
+
+		if tt.err != nil {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+			assert.Equal(tt.exp, mp)
+		}
+
+	}
+}
