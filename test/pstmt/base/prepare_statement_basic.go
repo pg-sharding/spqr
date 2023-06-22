2695db90efbe1aec4d3cd99670daaf2b9dcec34a
reshke <reshke@double.cloud>
2023-06-22 11:37:42 +0500

    Restore testing utility (#169)
    * Restore testing utility
    
    * Fixes and bump go
    
    * Add no lint

---
+++ test/pstmt/base/prepare_statement_basic.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/jackc/pgproto3/v2"
+	"github.com/pg-sharding/spqr/pkg/spqrlog"
+)
+
+var readResp = flag.Bool("v", false, "Logs every packet in great detail")
+var doErr = flag.Bool("e", false, "Logs every packet in great detail")
+
+func getC() (net.Conn, error) {
+	const proto = "tcp"
+	const addr = "[::1]:6432"
+	return net.Dial(proto, addr)
+}
+
+var okerr = errors.New("something")
+
+// nolint
+func readCnt(fr *pgproto3.Frontend, count int) error {
+	for i := 0; i < count; i++ {
+		if msg, err := fr.Receive(); err != nil {
+			return err
+		} else {
+			spqrlog.Logger.Printf(spqrlog.INFO, "received %T msg", msg)
+		}
+	}
+
+	return nil
+}
+
+func waitRFQ(fr *pgproto3.Frontend) error {
+	for {
+		if msg, err := fr.Receive(); err != nil {
+			return err
+		} else {
+			spqrlog.Logger.Printf(spqrlog.INFO, "received %+v msg", msg)
+			switch msg.(type) {
+			case *pgproto3.ErrorResponse:
+				return okerr
+			case *pgproto3.ReadyForQuery:
+				return nil
+			}
+		}
+	}
+}
+
+func prepLong(fr *pgproto3.Frontend, waitforres bool) error {
+	if *doErr {
+		if err := fr.Send(&pgproto3.Parse{
+			Name:  "s1",
+			Query: "SELECT 1/0",
+		}); err != nil {
+			return err
+		}
+	} else {
+		if err := fr.Send(&pgproto3.Parse{
+			Name:  "s1",
+			Query: "SELECT 1",
+		}); err != nil {
+			return err
+		}
+	}
+	if err := fr.Send(&pgproto3.Describe{
+		Name:       "s1",
+		ObjectType: byte('S'),
+	}); err != nil {
+		return err
+	}
+
+	if err := fr.Send(&pgproto3.Sync{}); err != nil {
+		return err
+	}
+
+	spqrlog.Logger.Printf(spqrlog.INFO, "reading prep parse")
+	if err := waitRFQ(fr); err != nil {
+		return err
+	}
+
+	if err := fr.Send(&pgproto3.Bind{
+		PreparedStatement: "s1",
+	}); err != nil {
+		return err
+	}
+
+	if err := fr.Send(&pgproto3.Execute{}); err != nil {
+		return err
+	}
+
+	if err := fr.Send(&pgproto3.Sync{}); err != nil {
+		return err
+	}
+
+	if !waitforres {
+		spqrlog.Logger.Printf(spqrlog.INFO, "not reading prep resp")
+		return nil
+	}
+
+	spqrlog.Logger.Printf(spqrlog.INFO, "reading prep resp")
+	return waitRFQ(fr)
+}
+
+func gaogao(wg *sync.WaitGroup, waitforres bool) {
+	defer wg.Done()
+
+	conn, err := getC()
+	if err != nil {
+		spqrlog.Logger.Printf(spqrlog.ERROR, "failed to get conn %v", err)
+		if err != okerr {
+			panic(err)
+		}
+		return
+	}
+	defer conn.Close()
+
+	frontend := pgproto3.NewFrontend(pgproto3.NewChunkReader(conn), conn)
+
+	if err := frontend.Send(&pgproto3.StartupMessage{
+		ProtocolVersion: 196608,
+		Parameters: map[string]string{
+			"user":     "user1",
+			"database": "db1",
+			"password": "12345678",
+		},
+	}); err != nil {
+		spqrlog.Logger.Printf(spqrlog.ERROR, "startup failed %v", err)
+		if err != okerr {
+			panic(err)
+		}
+	}
+
+	time.Sleep(200 * time.Millisecond)
+
+	if err := waitRFQ(frontend); err != nil {
+		spqrlog.Logger.Printf(spqrlog.ERROR, "startup failed %v", err)
+		if err != okerr {
+			panic(err)
+		}
+		return
+	}
+
+	if err := prepLong(frontend, waitforres); err != nil {
+		spqrlog.Logger.Printf(spqrlog.ERROR, "prep failed %v", err)
+		if err != okerr {
+			panic(err)
+		}
+		return
+	}
+
+	spqrlog.Logger.Printf(spqrlog.INFO, "ok")
+}
+
+func main() {
+	flag.Parse()
+
+	wg := sync.WaitGroup{}
+	cnt := 1
+	wg.Add(cnt)
+	for i := 0; i < cnt; i++ {
+		go gaogao(&wg, *readResp)
+	}
+
+	wg.Wait()
+}
---
+++ test/stress/stress.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/pg-sharding/spqr/pkg/spqrlog"
+	"github.com/spf13/cobra"
+)
+
+const (
+	spqrPort = 6432
+)
+
+var (
+	username string
+	dbname   string
+	relation string
+	hostname string
+	sslmode  string
+)
+
+func getConn(ctx context.Context, dbname string, retryCnt int) (*sqlx.DB, error) {
+	pgConString := fmt.Sprintf("host=%s port=%d dbname=%s sslmode=%v user=%s", hostname, spqrPort, dbname, sslmode, username)
+	fmt.Printf("using connstring %s\n", pgConString)
+	for i := 0; i < retryCnt; i++ {
+		db, err := sqlx.ConnectContext(ctx, "postgres", pgConString)
+		if err != nil {
+			spqrlog.Logger.PrintError(fmt.Errorf("error while connecting to postgresql: %w", err))
+			continue
+		}
+		return db, nil
+	}
+	return nil, fmt.Errorf("failed to get database connection")
+}
+
+var r = rand.New(rand.NewSource(31337))
+
+func simple() {
+	ctx := context.TODO()
+
+	for {
+		func() {
+			time.Sleep(time.Duration(50+r.Intn(10)) * time.Microsecond)
+
+			conn, err := getConn(ctx, dbname, 2)
+			if err != nil {
+				spqrlog.Logger.PrintError(fmt.Errorf("stress test FAILED %w", err))
+				panic(err)
+			}
+			defer func(conn *sqlx.DB) {
+				err := conn.Close()
+				if err != nil {
+					spqrlog.Logger.PrintError(err)
+				}
+			}(conn)
+
+			if _, err := conn.Query(fmt.Sprintf("SELECT * FROM %s WHERE i = %d", relation, 1+r.Intn(10))); err != nil {
+				spqrlog.Logger.PrintError(err)
+				panic(err)
+			}
+
+			fmt.Printf("SELECT OK\n")
+		}()
+	}
+}
+
+var par int
+
+var cmd = &cobra.Command{
+	Use:   "stress -p `parallel`",
+	Short: "SPQR stress test tool",
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		wg := &sync.WaitGroup{}
+
+		for i := 0; i < par; i++ {
+			wg.Add(1)
+			go func(wg *sync.WaitGroup) {
+				defer wg.Done()
+				simple()
+			}(wg)
+		}
+
+		wg.Wait()
+
+		return nil
+	},
+	SilenceUsage:  true,
+	SilenceErrors: true,
+}
+
+var cmdTest = &cobra.Command{
+	Use:   "test",
+	Short: "SPQR stress test tool",
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		ctx, f := context.WithTimeout(context.Background(), 10*time.Second)
+		defer f()
+
+		go simple()
+
+		<-ctx.Done()
+		return nil
+	},
+	SilenceUsage:  true,
+	SilenceErrors: true,
+}
+
+func init() {
+	cmd.PersistentFlags().IntVarP(&par, "parallel", "p", 10, "# of workers")
+	cmd.PersistentFlags().StringVarP(&hostname, "host", "", "spqr_router_1_1", "")
+	cmd.PersistentFlags().StringVarP(&relation, "rel", "r", "x", "")
+	cmd.PersistentFlags().StringVarP(&dbname, "dbname", "d", "dbtpcc", "")
+	cmd.PersistentFlags().StringVarP(&username, "usename", "u", "user1", "")
+	cmd.PersistentFlags().StringVarP(&sslmode, "sslmode", "s", "disable", "")
+
+	cmd.AddCommand(cmdTest)
+}
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		panic(err)
+	}
+}
