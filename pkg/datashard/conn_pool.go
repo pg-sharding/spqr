2e22d374d9f70dc1e6e6c80f7039e6fc683562f7
reshke <reshke@double.cloud>
2023-06-28 09:31:01 +0000

    Add read-write check fail reason

--- pkg/datashard/conn_pool.go
+++ pkg/datashard/conn_pool.go
@@ -102,7 +102,7 @@ func (c *cPool) Connection(
 			}
 		}
 
-		return fmt.Errorf("failed to get connection to host %s due to too much concurrent conections", host)
+		return fmt.Errorf("failed to get connection to host %s due to too much concurrent connections", host)
 	}(); err != nil {
 		return nil, err
 	}
@@ -227,21 +227,22 @@ func (s *InstancePoolImpl) List() []shard.Shard {
 
 var _ DBPool = &InstancePoolImpl{}
 
-func checkRw(sh shard.Shard) (bool, error) {
+func checkRw(sh shard.Shard) (bool, string, error) {
 	if err := sh.Send(&pgproto3.Query{
 		String: "SHOW transaction_read_only",
 	}); err != nil {
 		spqrlog.Logger.Errorf("shard %s encounter error while sending read-write check %v", sh.Name(), err)
-		return false, err
+		return false, "", err
 	}
 
 	res := false
+	reason := ""
 
 	for {
 		msg, err := sh.Receive()
 		if err != nil {
 			spqrlog.Logger.Printf(spqrlog.DEBUG5, "shard %p recieved error %v during check rw", sh, err)
-			return false, err
+			return false, reason, err
 		}
 		spqrlog.Logger.Printf(spqrlog.DEBUG5, "shard %p recieved %+v during check rw", sh, msg)
 		switch qt := msg.(type) {
@@ -249,16 +250,18 @@ func checkRw(sh shard.Shard) (bool, error) {
 			spqrlog.Logger.Printf(spqrlog.DEBUG5, "shard %p checking read-write: result datarow %+v", sh, qt)
 			if len(qt.Values) == 1 && len(qt.Values[0]) == 3 && qt.Values[0][0] == 'o' && qt.Values[0][1] == 'f' && qt.Values[0][2] == 'f' {
 				res = true
+			} else {
+				reason = fmt.Sprintf("transaction_read_only is %+v", qt.Values)
 			}
 
 		case *pgproto3.ReadyForQuery:
 			if txstatus.TXStatus(qt.TxStatus) != txstatus.TXIDLE {
 				spqrlog.Logger.Printf(spqrlog.DEBUG5, "shard %p got unsync connection while calculating rw %v", sh, qt.TxStatus)
-				return false, fmt.Errorf("connection unsync while acquirind it")
+				return false, reason, fmt.Errorf("connection unsync while acquirind it")
 			}
 
 			spqrlog.Logger.Printf(spqrlog.DEBUG5, "shard %p calculated rw res %+v", sh, res)
-			return res, nil
+			return res, reason, nil
 		}
 	}
 }
@@ -301,12 +304,12 @@ func (s *InstancePoolImpl) Connection(
 				spqrlog.Logger.Errorf("failed to get connection to %s for %s: %v ", host, clid, err)
 				continue
 			}
-			if ch, err := checkRw(shard); err != nil {
+			if ch, reason, err := checkRw(shard); err != nil {
 				total_msg += fmt.Sprintf("host %s: ", host) + err.Error()
 				_ = s.poolRO.Discard(shard)
 				continue
 			} else if ch {
-				total_msg += fmt.Sprintf("host %s: read-only check fail ", host)
+				total_msg += fmt.Sprintf("host %s: read-only check fail: %s ", host, reason)
 				_ = s.Put(shard)
 				continue
 			}
@@ -323,12 +326,12 @@ func (s *InstancePoolImpl) Connection(
 				spqrlog.Logger.Errorf("failed to get connection to %s for %s: %v ", host, clid, err)
 				continue
 			}
-			if ch, err := checkRw(shard); err != nil {
+			if ch, reason, err := checkRw(shard); err != nil {
 				total_msg += fmt.Sprintf("host %s: ", host) + err.Error()
 				_ = s.poolRO.Discard(shard)
 				continue
 			} else if !ch {
-				total_msg += fmt.Sprintf("host %s: read-write check fail ", host)
+				total_msg += fmt.Sprintf("host %s: read-write check fail: %s ", host, reason)
 				_ = s.Put(shard)
 				continue
 			}
