614250f4af044bcf4bdf7f85cb7a31982a3e9955
reshke <reshke@double.cloud>
2023-08-04 11:11:11 +0000

    Add negotiation tests

--- router/client/client_test.go
+++ router/client/client_test.go
@@ -2,6 +2,7 @@ package client_test
 
 import (
 	"encoding/binary"
+	"fmt"
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgproto3"
@@ -45,4 +46,46 @@ func TestCancel(t *testing.T) {
 	assert.Equal(uint32(7), client.CancelMsg().ProcessID)
 	assert.Equal(uint32(12), client.CancelMsg().SecretKey)
 	assert.NoError(err)
+}
+
+func TestNoGSSAPI(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+
+	rconn := mock_conn.NewMockRawConn(ctrl)
+
+	rconn.EXPECT().Read(gomock.Any()).DoAndReturn(
+		func(b []byte) (int, error) {
+			binary.BigEndian.PutUint32(b, 8)
+
+			return 4, nil
+		}).Times(1)
+
+	rconn.EXPECT().Read(gomock.Any()).DoAndReturn(
+		func(b []byte) (int, error) {
+
+			binary.BigEndian.PutUint32(b, conn.GSSREQ)
+			return 4, nil
+		}).Times(1)
+
+	exprErr := fmt.Errorf("stop test")
+
+	rconn.EXPECT().Read(gomock.Any()).DoAndReturn(
+		func(b []byte) (int, error) {
+
+			return 0, exprErr
+		}).Times(1)
+
+	rconn.EXPECT().Write(gomock.Any()).DoAndReturn(func(b []byte) (int, error) {
+
+		assert.Equal(1, len(b))
+
+		assert.Equal(uint8('N'), b[0])
+		return 4, nil
+	}).Times(1)
+
+	client := client.NewPsqlClient(rconn)
+
+	err := client.Init(nil)
+	assert.Equal(exprErr, err)
 }
