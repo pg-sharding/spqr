fdc2d0258d4b1f6ef2847185dda52e3659492625
reshke <reshke@double.cloud>
2023-08-07 14:48:41 +0500

    Cleanup mockgen. Move pgx tx/conn mocks to separate folder (#264)

--- pkg/datatransfers/pgx_tx_iface.go
+++ pkg/datatransfers/pgx_tx_iface.go
@@ -1,40 +1,10 @@
 package datatransfers
 
 import (
-	"context"
-
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	_ "github.com/lib/pq"
 )
 
 type Tx interface {
-	// Begin starts a pseudo nested transaction.
-	Begin(ctx context.Context) (pgx.Tx, error)
-
-	// Commit commits the transaction if this is a real transaction or releases the savepoint if this is a pseudo nested
-	// transaction. Commit will return an error where errors.Is(ErrTxClosed) is true if the Tx is already closed, but is
-	// otherwise safe to call multiple times. If the commit fails with a rollback status (e.g. the transaction was already
-	// in a broken state) then an error where errors.Is(ErrTxCommitRollback) is true will be returned.
-	Commit(ctx context.Context) error
-
-	// Rollback rolls back the transaction if this is a real transaction or rolls back to the savepoint if this is a
-	// pseudo nested transaction. Rollback will return an error where errors.Is(ErrTxClosed) is true if the Tx is already
-	// closed, but is otherwise safe to call multiple times. Hence, a defer tx.Rollback() is safe even if tx.Commit() will
-	// be called first in a non-error condition. Any other failure of a real transaction will result in the connection
-	// being closed.
-	Rollback(ctx context.Context) error
-
-	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
-	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
-	LargeObjects() pgx.LargeObjects
-
-	Prepare(ctx context.Context, name, sql string) (*pgconn.StatementDescription, error)
-
-	Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
-	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
-	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
-
-	// Conn returns the underlying *Conn that on which this transaction is executing.
-	Conn() *pgx.Conn
+	pgx.Tx
 }
