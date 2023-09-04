8b7e5ce1182d957cb673e55fc0a8bddf447fe938
Stepan Filippov <stepan.filippov@yandex.ru>
2023-09-04 16:55:11 +0500

    replay logs

--- cmd/logproxy/main.go
+++ cmd/logproxy/main.go
@@ -11,6 +11,7 @@ var (
 	port   string
 	user   string
 	dbname string
+	file   string
 )
 
 var rootCmd = &cobra.Command{
@@ -22,7 +23,7 @@ var rootCmd = &cobra.Command{
 	SilenceErrors: false,
 }
 
-var addRouterCmd = &cobra.Command{
+var startProxySessionCmd = &cobra.Command{
 	Use:   "run",
 	Short: "start proxy log writing session",
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -35,14 +36,38 @@ var addRouterCmd = &cobra.Command{
 	SilenceErrors: false,
 }
 
+var replayLogsCmd = &cobra.Command{
+	Use:   "replay",
+	Short: "replay written logs to db",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		prox := logproxy.NewProxy(host, port)
+		err := prox.ReplayLogs(host, port, user, file, dbname)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+	SilenceUsage:  false,
+	SilenceErrors: false,
+}
+
 func init() {
-	addRouterCmd.PersistentFlags().StringVarP(&host, "host", "H", "localhost", `database server host (default: "localhost")`)
-	addRouterCmd.PersistentFlags().StringVarP(&port, "port", "p", "5432", `database server port (default: 5432)`)
-	addRouterCmd.PersistentFlags().StringVarP(&user, "user", "U", "postgres", `database server user (default: postgres)`)
-	addRouterCmd.PersistentFlags().StringVarP(&dbname, "dbname", "d", "postgres", `database name to connect to (default: postgres)`)
+	startProxySessionCmd.PersistentFlags().StringVarP(&host, "host", "H", "localhost", `database server host (default: "localhost")`)
+	startProxySessionCmd.PersistentFlags().StringVarP(&port, "port", "p", "5432", `database server port (default: 5432)`)
+	startProxySessionCmd.PersistentFlags().StringVarP(&user, "user", "U", "postgres", `database server user (default: postgres)`)
+	startProxySessionCmd.PersistentFlags().StringVarP(&dbname, "dbname", "d", "postgres", `database name to connect to (default: postgres)`)
+
+	replayLogsCmd.PersistentFlags().StringVarP(&host, "host", "H", "localhost", `database server host (default: "localhost")`)
+	replayLogsCmd.PersistentFlags().StringVarP(&port, "port", "p", "5432", `database server port (default: 5432)`)
+	replayLogsCmd.PersistentFlags().StringVarP(&user, "user", "U", "postgres", `database server user (default: postgres)`)
+	replayLogsCmd.PersistentFlags().StringVarP(&dbname, "dbname", "d", "postgres", `database name to connect to (default: postgres)`)
+	replayLogsCmd.PersistentFlags().StringVarP(&file, "logfile", "l", "", `file to read logs from`)
 
 	/* --- Router cmds --- */
-	rootCmd.AddCommand(addRouterCmd)
+	rootCmd.AddCommand(startProxySessionCmd)
+	rootCmd.AddCommand(replayLogsCmd)
 	/* ------------------- */
 }
 
--- test/logproxy/logproxy.go
+++ test/logproxy/logproxy.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"sync"
@@ -62,6 +63,7 @@ func (p *Proxy) Run() error {
 	for {
 		select {
 		case <-ctx.Done():
+			p.Flush()
 			os.Exit(1)
 		case c := <-cChan:
 
@@ -95,6 +97,7 @@ func (p *Proxy) serv(netconn net.Conn) error {
 		if err != nil {
 			return fmt.Errorf("failed to receive msg from client %w", err)
 		}
+		fmt.Printf("message type %T\n", msg)
 
 		//writing request data to buffer
 		byt, err := encodeMessage(msg)
@@ -150,6 +153,159 @@ func (p *Proxy) Flush() error {
 	}
 
 	return nil
+}
+
+func (p *Proxy) ReplayLogs(host string, port string, user string, file string, db string) error {
+	spqrlog.Zero.Debug().Msg("started parsing")
+	// TODO connect to db
+	startupMessage := &pgproto3.StartupMessage{
+		ProtocolVersion: pgproto3.ProtocolVersionNumber,
+		Parameters: map[string]string{
+			"user":     user,
+			"database": db,
+		},
+	}
+	spqrlog.Zero.Debug().Msg("created startup")
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", p.host, p.port))
+	if err != nil {
+		return err
+	}
+
+	frontend := pgproto3.NewFrontend(bufio.NewReader(conn), conn)
+
+	frontend.Send(startupMessage)
+	if err := frontend.Flush(); err != nil {
+		return fmt.Errorf("failed to send msg to bd %w", err)
+	}
+	spqrlog.Zero.Debug().Msg("send startup")
+	for {
+		//recieve response
+		retmsg, err := frontend.Receive()
+		if err != nil {
+			return fmt.Errorf("failed to receive msg from db %w", err)
+		}
+
+		if shouldStop(retmsg) {
+			break
+		}
+	}
+
+	spqrlog.Zero.Debug().Msg("connected")
+	//TODO parse file (partially?)
+	f, err := os.OpenFile(file, os.O_RDONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	spqrlog.Zero.Debug().Msg("file open")
+
+	for {
+		tim, msg, err := parseFile(f)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+
+		//TODO send requests in correct time
+		fmt.Println(tim.String())
+		//smth with time
+
+		frontend.Send(msg)
+		if err := frontend.Flush(); err != nil {
+			return fmt.Errorf("failed to send msg to bd %w", err)
+		}
+		spqrlog.Zero.Debug().Msg("message sent")
+		for {
+			spqrlog.Zero.Debug().Msg("for")
+			//recieve response
+			retmsg, err := frontend.Receive()
+			if err != nil {
+				return fmt.Errorf("failed to receive msg from db %w", err)
+			}
+			fmt.Printf("wtf: %T  %+v\n", retmsg, retmsg)
+			spqrlog.Zero.Debug().Msg("recieved from frontend")
+
+			if shouldStop(retmsg) {
+				spqrlog.Zero.Debug().Msg("stop")
+				break
+			}
+			spqrlog.Zero.Debug().Msg("did not stop")
+		}
+	}
+}
+
+func parseFile(f *os.File) (time.Time, pgproto3.FrontendMessage, error) {
+	// 15 byte - timestamp
+	// 1 byte - message header
+	// 4 bytes - message length (except header)
+	// ?? bytes - message bytes
+
+	//timestamp
+	timeb := make([]byte, 15)
+	_, err := f.Read(timeb) //err
+	if err != nil {
+		spqrlog.Zero.Debug().Msg("f timestamp reading")
+		spqrlog.Zero.Debug().Msg(err.Error())
+		return time.Now(), nil, err
+	}
+	var ti time.Time
+	err = ti.UnmarshalBinary(timeb)
+	if err != nil {
+		spqrlog.Zero.Debug().Msg("f timestamp")
+		spqrlog.Zero.Debug().Msg(err.Error())
+		return time.Now(), nil, err
+	}
+	fmt.Printf("time: %s", ti.String())
+	spqrlog.Zero.Debug().Msg("timestamp ok")
+
+	//type
+	tip := make([]byte, 1)
+
+	_, err = f.Read(tip)
+	if err != nil {
+		return time.Now(), nil, err
+	}
+	if string(tip) == "X" {
+		return time.Now(), nil, io.EOF
+	}
+	fmt.Printf("tip: %s", string(tip))
+	spqrlog.Zero.Debug().Msg("tip?")
+
+	//size
+	rawSize := make([]byte, 4)
+
+	_, err = f.Read(rawSize)
+	if err != nil {
+		return time.Now(), nil, err
+	}
+
+	msgSize := int(binary.BigEndian.Uint32(rawSize) - 4)
+	fmt.Printf("size: %d", msgSize)
+	spqrlog.Zero.Debug().Msg("size?")
+
+	//message
+	msg := make([]byte, msgSize)
+	_, err = f.Read(msg)
+	if err != nil {
+		return time.Now(), nil, err
+	}
+	fmt.Printf("message: %s", string(msg))
+	spqrlog.Zero.Debug().Msg("message")
+
+	message := append(tip, rawSize...)
+	message = append(message, msg...)
+	spqrlog.Zero.Debug().Msg(string(message))
+
+	fm := &pgproto3.Query{}
+	err = fm.Decode(msg)
+	//err = fm.Decode(message)
+	if err != nil {
+		return time.Now(), nil, err
+	}
+
+	return ti, fm, nil
 }
 
 func startup(netconn net.Conn, frontend *pgproto3.Frontend, cl *pgproto3.Backend) error {
@@ -241,8 +397,15 @@ Gets pgproto3.FrontendMessage and encodes it in binary with timestamp.
 ?? bytes - message bytes
 */
 func encodeMessage(msg pgproto3.FrontendMessage) ([]byte, error) {
-	var b1 []byte
-	b2 := msg.Encode(b1)
+	//fmt.Printf("message type %T\n", msg)
+	b2 := msg.Encode(nil)
+	//spqrlog.Zero.Debug().Msg(string(b2))
+	// fm := &pgproto3.Query{}
+	// fmt.Printf("message type %T\n", fm)
+	// er := fm.Decode(b2[5:])
+	// if er != nil {
+	// 	panic(er)
+	// }
 
 	t := time.Now()
 	tb, err := t.MarshalBinary()
