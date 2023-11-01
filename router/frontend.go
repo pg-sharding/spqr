fe4670c532218ceefc1cedb118e97260e822d489
reshke <reshke@double.cloud>
2023-11-01 17:16:20 +0500

    Bump lyx (#312)
    * Bump luyx
    
    * Fix
    
    * Better

--- router/frontend.go
+++ router/frontend.go
@@ -349,7 +349,7 @@ func Frontend(qr qrouter.QueryRouter, cl client.RouterClient, cmngr poolmgr.Pool
 				// ok
 			default:
 				spqrlog.Zero.Error().
-					Uint("client", spqrlog.GetPointer(rst.Client())).Int("tx-status", int(rst.TxStatus())).
+					Uint("client", spqrlog.GetPointer(rst.Client())).Int("tx-status", int(rst.TxStatus())).Err(err).
 					Msg("client iteration done with error")
 				if err := rst.UnRouteWithError(rst.ActiveShards(), fmt.Errorf("client proccessing error: %v, tx status %s", err, rst.TxStatus().String())); err != nil {
 					return err
