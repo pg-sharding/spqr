e06c16b864a62d1d35e4764c4f72ffe5caa0f90a
reshke <reshke@double.cloud>
2023-11-09 10:46:37 +0500

    Accept routing hints in comments (#317)
    * Accept routing hints in comments

--- router/client/client_test.go
+++ router/client/client_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/jackc/pgx/v5/pgproto3"
 	"github.com/pg-sharding/spqr/router/client"
+	"go.uber.org/mock/gomock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/pg-sharding/spqr/pkg/conn"
