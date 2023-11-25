014dd313f90c81709bb00641f438117a1625aca5
cbist <dsync@list.ru>
2023-11-25 17:42:01 +0500

    add test to router_hint

---
+++ test/regress/tests/router/sql/routing_hint.sql
@@ -0,0 +1,11 @@
+\c spqr-console
+
+ADD SHARDING RULE t1 COLUMNS id;
+ADD KEY RANGE krid1 FROM 1 TO 11 ROUTE TO sh1;
+ADD KEY RANGE krid2 FROM 11 TO 101 ROUTE TO sh2;
+
+\c regress
+
+CREATE TABLE test(id int, age int);
+INSERT INTO test(id, age) VALUES (10, 16) /*sharding_key: 30*/;
+INSERT INTO test(id, age) VALUES (10, 16) /*sharding_key: 3000*/;
