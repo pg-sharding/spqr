a020ece35e0b12d6cc8ff7b396f9a407298a2c34
reshke <reshke@double.cloud>
2024-01-15 17:28:26 +0000

    proper gen

--- yacc/console/gram.go
+++ yacc/console/gram.go
@@ -222,6 +222,7 @@ var yyToknames = [...]string{
 	"TYPES",
 	"OP",
 }
+
 var yyStatenames = [...]string{}
 
 const yyEofCode = 1
@@ -231,7 +232,7 @@ const yyInitialStackSize = 16
 //line gram.y:725
 
 //line yacctab:1
-var yyExca = [...]int{
+var yyExca = [...]int8{
 	-1, 1,
 	1, -1,
 	-2, 0,
@@ -241,8 +242,7 @@ const yyPrivate = 57344
 
 const yyLast = 231
 
-var yyAct = [...]int{
-
+var yyAct = [...]uint8{
 	123, 157, 67, 174, 120, 94, 132, 107, 131, 155,
 	27, 28, 57, 175, 176, 177, 113, 85, 52, 51,
 	159, 127, 30, 29, 34, 35, 134, 66, 21, 20,
@@ -268,8 +268,8 @@ var yyAct = [...]int{
 	7, 11, 10, 64, 62, 55, 2, 122, 148, 143,
 	92,
 }
-var yyPact = [...]int{
 
+var yyPact = [...]int16{
 	4, -1000, 122, -1000, -1000, -1000, -1000, -1000, -1000, -1000,
 	-1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000,
 	77, 77, -47, -48, 51, 45, 45, 174, 23, 148,
@@ -293,16 +293,16 @@ var yyPact = [...]int{
 	170, 170, 170, 170, -39, -39, -39, -39, -1000, -1000,
 	-1000, -1000,
 }
-var yyPgo = [...]int{
 
+var yyPgo = [...]uint8{
 	0, 230, 4, 229, 228, 227, 2, 0, 5, 226,
 	225, 133, 12, 224, 223, 222, 221, 220, 219, 218,
 	217, 216, 215, 149, 146, 142, 134, 8, 6, 7,
 	214, 213, 212, 1, 211, 210, 3, 205, 200, 199,
 	198, 197, 196, 195, 194, 193, 192, 187,
 }
-var yyR1 = [...]int{
 
+var yyR1 = [...]int8{
 	0, 46, 47, 47, 9, 9, 9, 9, 9, 9,
 	9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
 	9, 8, 6, 6, 6, 7, 3, 3, 3, 4,
@@ -315,8 +315,8 @@ var yyR1 = [...]int{
 	26, 26, 37, 10, 11, 12, 41, 16, 16, 42,
 	43, 40, 39, 44, 45, 45,
 }
-var yyR2 = [...]int{
 
+var yyR2 = [...]int8{
 	0, 2, 0, 1, 1, 1, 1, 1, 1, 1,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -329,8 +329,8 @@ var yyR2 = [...]int{
 	5, 4, 2, 3, 3, 2, 6, 3, 3, 4,
 	4, 2, 1, 5, 3, 3,
 }
-var yyChk = [...]int{
 
+var yyChk = [...]int16{
 	-1000, -46, -9, -18, -19, -20, -21, -17, -38, -37,
 	-15, -16, -40, -39, -41, -42, -43, -44, -45, -22,
 	25, 24, 64, 65, 26, 27, 28, 6, 7, 19,
@@ -354,8 +354,8 @@ var yyChk = [...]int{
 	54, 54, 54, 54, -7, -7, -7, -7, -33, -33,
 	-33, -33,
 }
-var yyDef = [...]int{
 
+var yyDef = [...]int8{
 	0, -2, 2, 4, 5, 6, 7, 8, 9, 10,
 	11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -379,12 +379,12 @@ var yyDef = [...]int{
 	0, 0, 0, 0, 85, 85, 85, 85, 88, 89,
 	86, 87,
 }
-var yyTok1 = [...]int{
 
+var yyTok1 = [...]int8{
 	1,
 }
-var yyTok2 = [...]int{
 
+var yyTok2 = [...]int8{
 	2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
 	12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
 	22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
@@ -394,7 +394,8 @@ var yyTok2 = [...]int{
 	62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
 	72,
 }
-var yyTok3 = [...]int{
+
+var yyTok3 = [...]int8{
 	0,
 }
 
@@ -476,9 +477,9 @@ func yyErrorMessage(state, lookAhead int) string {
 	expected := make([]int, 0, 4)
 
 	// Look for shiftable tokens.
-	base := yyPact[state]
+	base := int(yyPact[state])
 	for tok := TOKSTART; tok-1 < len(yyToknames); tok++ {
-		if n := base + tok; n >= 0 && n < yyLast && yyChk[yyAct[n]] == tok {
+		if n := base + tok; n >= 0 && n < yyLast && int(yyChk[int(yyAct[n])]) == tok {
 			if len(expected) == cap(expected) {
 				return res
 			}
@@ -488,13 +489,13 @@ func yyErrorMessage(state, lookAhead int) string {
 
 	if yyDef[state] == -2 {
 		i := 0
-		for yyExca[i] != -1 || yyExca[i+1] != state {
+		for yyExca[i] != -1 || int(yyExca[i+1]) != state {
 			i += 2
 		}
 
 		// Look for tokens that we accept or reduce.
 		for i += 2; yyExca[i] >= 0; i += 2 {
-			tok := yyExca[i]
+			tok := int(yyExca[i])
 			if tok < TOKSTART || yyExca[i+1] == 0 {
 				continue
 			}
@@ -525,30 +526,30 @@ func yylex1(lex yyLexer, lval *yySymType) (char, token int) {
 	token = 0
 	char = lex.Lex(lval)
 	if char <= 0 {
-		token = yyTok1[0]
+		token = int(yyTok1[0])
 		goto out
 	}
 	if char < len(yyTok1) {
-		token = yyTok1[char]
+		token = int(yyTok1[char])
 		goto out
 	}
 	if char >= yyPrivate {
 		if char < yyPrivate+len(yyTok2) {
-			token = yyTok2[char-yyPrivate]
+			token = int(yyTok2[char-yyPrivate])
 			goto out
 		}
 	}
 	for i := 0; i < len(yyTok3); i += 2 {
-		token = yyTok3[i+0]
+		token = int(yyTok3[i+0])
 		if token == char {
-			token = yyTok3[i+1]
+			token = int(yyTok3[i+1])
 			goto out
 		}
 	}
 
 out:
 	if token == 0 {
-		token = yyTok2[1] /* unknown char */
+		token = int(yyTok2[1]) /* unknown char */
 	}
 	if yyDebug >= 3 {
 		__yyfmt__.Printf("lex %s(%d)\n", yyTokname(token), uint(char))
@@ -603,7 +604,7 @@ yystack:
 	yyS[yyp].yys = yystate
 
 yynewstate:
-	yyn = yyPact[yystate]
+	yyn = int(yyPact[yystate])
 	if yyn <= yyFlag {
 		goto yydefault /* simple state */
 	}
@@ -614,8 +615,8 @@ yynewstate:
 	if yyn < 0 || yyn >= yyLast {
 		goto yydefault
 	}
-	yyn = yyAct[yyn]
-	if yyChk[yyn] == yytoken { /* valid shift */
+	yyn = int(yyAct[yyn])
+	if int(yyChk[yyn]) == yytoken { /* valid shift */
 		yyrcvr.char = -1
 		yytoken = -1
 		yyVAL = yyrcvr.lval
@@ -628,7 +629,7 @@ yynewstate:
 
 yydefault:
 	/* default state action */
-	yyn = yyDef[yystate]
+	yyn = int(yyDef[yystate])
 	if yyn == -2 {
 		if yyrcvr.char < 0 {
 			yyrcvr.char, yytoken = yylex1(yylex, &yyrcvr.lval)
@@ -637,18 +638,18 @@ yydefault:
 		/* look through exception table */
 		xi := 0
 		for {
-			if yyExca[xi+0] == -1 && yyExca[xi+1] == yystate {
+			if yyExca[xi+0] == -1 && int(yyExca[xi+1]) == yystate {
 				break
 			}
 			xi += 2
 		}
 		for xi += 2; ; xi += 2 {
-			yyn = yyExca[xi+0]
+			yyn = int(yyExca[xi+0])
 			if yyn < 0 || yyn == yytoken {
 				break
 			}
 		}
-		yyn = yyExca[xi+1]
+		yyn = int(yyExca[xi+1])
 		if yyn < 0 {
 			goto ret0
 		}
@@ -670,10 +671,10 @@ yydefault:
 
 			/* find a state where "error" is a legal shift action */
 			for yyp >= 0 {
-				yyn = yyPact[yyS[yyp].yys] + yyErrCode
+				yyn = int(yyPact[yyS[yyp].yys]) + yyErrCode
 				if yyn >= 0 && yyn < yyLast {
-					yystate = yyAct[yyn] /* simulate a shift of "error" */
-					if yyChk[yystate] == yyErrCode {
+					yystate = int(yyAct[yyn]) /* simulate a shift of "error" */
+					if int(yyChk[yystate]) == yyErrCode {
 						goto yystack
 					}
 				}
@@ -709,7 +710,7 @@ yydefault:
 	yypt := yyp
 	_ = yypt // guard against "declared and not used"
 
-	yyp -= yyR2[yyn]
+	yyp -= int(yyR2[yyn])
 	// yyp is now the index of $0. Perform the default action. Iff the
 	// reduced production is ε, $1 is possibly out of range.
 	if yyp+1 >= len(yyS) {
@@ -720,16 +721,16 @@ yydefault:
 	yyVAL = yyS[yyp+1]
 
 	/* consult goto table to find next state */
-	yyn = yyR1[yyn]
-	yyg := yyPgo[yyn]
+	yyn = int(yyR1[yyn])
+	yyg := int(yyPgo[yyn])
 	yyj := yyg + yyS[yyp].yys + 1
 
 	if yyj >= yyLast {
-		yystate = yyAct[yyg]
+		yystate = int(yyAct[yyg])
 	} else {
-		yystate = yyAct[yyj]
-		if yyChk[yystate] != -yyn {
-			yystate = yyAct[yyg]
+		yystate = int(yyAct[yyj])
+		if int(yyChk[yystate]) != -yyn {
+			yystate = int(yyAct[yyg])
 		}
 	}
 	// dummy call; replaced with literal code
--- yacc/console/lex.go
+++ yacc/console/lex.go
@@ -10,7 +10,7 @@ import (
 
 
 
-//line lex.go:14
+//line lex.go:12
 const lexer_start int = 4
 const lexer_first_final int = 4
 const lexer_error int = 0
@@ -36,7 +36,7 @@ func NewLexer(data []byte) *Lexer {
         pe: len(data),
     }
     
-//line lex.go:40
+//line lex.go:36
 	{
 	 lex.cs = lexer_start
 	 lex.ts = 0
@@ -52,7 +52,7 @@ func ResetLexer(lex *Lexer, data []byte) {
     lex.pe = len(data)
     lex.data = data
     
-//line lex.go:56
+//line lex.go:50
 	{
 	 lex.cs = lexer_start
 	 lex.ts = 0
@@ -73,7 +73,7 @@ func (lex *Lexer) Lex(lval *yySymType) int {
     var tok int
 
     
-//line lex.go:77
+//line lex.go:69
 	{
 	if ( lex.p) == ( lex.pe) {
 		goto _test_eof
@@ -197,7 +197,7 @@ tr21:
 //line NONE:1
  lex.ts = ( lex.p)
 
-//line lex.go:201
+//line lex.go:193
 		switch  lex.data[( lex.p)] {
 		case 32:
 			goto st5
@@ -297,7 +297,7 @@ tr14:
 			goto _test_eof6
 		}
 	st_case_6:
-//line lex.go:301
+//line lex.go:293
 		switch  lex.data[( lex.p)] {
 		case 33:
 			goto tr8
@@ -346,7 +346,7 @@ tr16:
 			goto _test_eof7
 		}
 	st_case_7:
-//line lex.go:350
+//line lex.go:342
 		switch  lex.data[( lex.p)] {
 		case 34:
 			goto tr16
@@ -521,7 +521,7 @@ tr12:
 			goto _test_eof12
 		}
 	st_case_12:
-//line lex.go:525
+//line lex.go:517
 		switch  lex.data[( lex.p)] {
 		case 34:
 			goto st8
@@ -587,7 +587,7 @@ tr5:
 			goto _test_eof13
 		}
 	st_case_13:
-//line lex.go:591
+//line lex.go:583
 		if  lex.data[( lex.p)] == 42 {
 			goto st3
 		}
