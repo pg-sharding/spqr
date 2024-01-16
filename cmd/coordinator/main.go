bfe2d66107a7ae6f32c8ef5c3d99c0eb356ea2b3
Denis Volkov <3149929+Denchick@users.noreply.github.com>
2024-01-16 15:01:56 +0500

    add GOMAXPROCS in coordinator app (#407)

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -3,25 +3,37 @@ package main
 import (
 	"github.com/pg-sharding/spqr/coordinator/app"
 	"github.com/pg-sharding/spqr/coordinator/provider"
+	"github.com/pg-sharding/spqr/pkg"
 	"github.com/pg-sharding/spqr/pkg/config"
 	"github.com/pg-sharding/spqr/pkg/spqrlog"
 	"github.com/pg-sharding/spqr/qdb"
 	"github.com/spf13/cobra"
+	"runtime"
 )
 
-var cfgPath string
-var qdbImpl string
+var (
+	cfgPath    string
+	qdbImpl    string
+	gomaxprocs int
+)
 
 var rootCmd = &cobra.Command{
-	Use: "spqr-coordinator --config `path-to-config`",
+	Use:   "spqr-coordinator run --config `path-to-config`",
+	Short: "sqpr-coordinator",
+	Long:  "spqr-coordinator",
 	CompletionOptions: cobra.CompletionOptions{
 		DisableDefaultCmd: true,
 	},
+	Version:       pkg.SpqrVersionRevision,
 	SilenceUsage:  true,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := config.LoadCoordinatorCfg(cfgPath); err != nil {
 			return err
+		}
+
+		if gomaxprocs > 0 {
+			runtime.GOMAXPROCS(gomaxprocs)
 		}
 
 		db, err := qdb.NewXQDB(qdbImpl)
@@ -38,6 +50,7 @@ var rootCmd = &cobra.Command{
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&cfgPath, "config", "c", "/etc/spqr/coordinator.yaml", "path to config file")
 	rootCmd.PersistentFlags().StringVarP(&qdbImpl, "qdb-impl", "", "etcd", "which implementation of QDB to use.")
+	rootCmd.PersistentFlags().IntVarP(&gomaxprocs, "gomaxprocs", "", 0, "GOMAXPROCS value")
 }
 
 func main() {
