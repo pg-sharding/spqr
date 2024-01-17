58ca5d5af99c88078d84e74d1df9c2e002c540af
Yury Frolov <yura200253@gmail.com>
2024-01-17 16:33:27 +0500

    Added xproto test for prepared insert with params

--- test/xproto/proto_test.go
+++ test/xproto/proto_test.go
@@ -1,7 +1,10 @@
 package prep_stmt_test
 
 import (
+	"context"
+	"encoding/binary"
 	"fmt"
+	"github.com/jackc/pgx/v5"
 	"net"
 	"os"
 	"testing"
@@ -66,6 +69,91 @@ func getConnectionParams() map[string]string {
 		res["password"] = password
 	}
 	return res
+}
+
+func SetupSharding() {
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "[::1]"
+	}
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "6432"
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s dbname=%s port=%s",
+		host,
+		"spqr-console",
+		"spqr-console",
+		port,
+	)
+
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = conn.Close(context.Background())
+	}()
+
+	_, err = conn.Exec(context.Background(), "CREATE SHARDING RULE r1 COLUMNS id;")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "could not setup sharding: %s", err)
+	}
+	_, err = conn.Exec(context.Background(), "Add KEY RANGE krid1 FROM 1 TO 11 ROUTE TO sh1;")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "could not setup sharding: %s", err)
+	}
+	_, err = conn.Exec(context.Background(), "Add KEY RANGE krid2 FROM 11 TO 21 ROUTE TO sh2;")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "could not setup sharding: %s", err)
+	}
+}
+
+func CreateTable() {
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "[::1]"
+	}
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "6432"
+	}
+
+	params := getConnectionParams()
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s dbname=%s port=%s",
+		host,
+		params["user"],
+		params["database"],
+		port,
+	)
+
+	if val, ok := params["password"]; ok {
+		dsn = dsn + " password=" + val
+	}
+
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		panic(fmt.Errorf("failed to connect to database: %s", err))
+	}
+	defer func() {
+		_ = conn.Close(context.Background())
+	}()
+
+	_, err = conn.Exec(context.Background(), "CREATE TABLE t (id int)")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "could not create table: %s", err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	SetupSharding()
+	CreateTable()
+	code := m.Run()
+	os.Exit(code)
 }
 
 func TestPrepStmt(t *testing.T) {
@@ -194,6 +282,82 @@ func TestPrepStmt(t *testing.T) {
 				},
 				&pgproto3.CommandComplete{
 					CommandTag: []byte("SELECT 1"),
+				},
+				&pgproto3.ReadyForQuery{
+					TxStatus: 73, /*txidle*/
+				},
+			},
+		},
+		{
+			Request: []pgproto3.FrontendMessage{
+				&pgproto3.Parse{
+					Name:  "stmt3",
+					Query: "INSERT INTO t (\"id\") values ($1) RETURNING \"id\"",
+				},
+				&pgproto3.Describe{
+					ObjectType: 'S',
+					Name:       "stmt3",
+				},
+				&pgproto3.Bind{
+					PreparedStatement:    "stmt3",
+					DestinationPortal:    "",
+					ParameterFormatCodes: []int16{pgproto3.BinaryFormat},
+					Parameters: [][]byte{
+						func() []byte {
+							res := make([]byte, 4)
+							binary.BigEndian.PutUint32(res, 1)
+							return res
+						}(),
+					},
+					ResultFormatCodes: []int16{pgproto3.BinaryFormat},
+				},
+				&pgproto3.Describe{
+					ObjectType: 'P',
+					Name:       "",
+				},
+				&pgproto3.Execute{},
+				&pgproto3.Sync{},
+			},
+			Response: []pgproto3.BackendMessage{
+				&pgproto3.ParseComplete{},
+				&pgproto3.ParameterDescription{
+					ParameterOIDs: []uint32{23},
+				},
+				&pgproto3.RowDescription{
+					Fields: []pgproto3.FieldDescription{
+						{
+							Name:                 []byte("id"),
+							DataTypeOID:          23,
+							DataTypeSize:         4,
+							TypeModifier:         -1,
+							TableAttributeNumber: 1,
+						},
+					},
+				},
+				&pgproto3.BindComplete{},
+				&pgproto3.RowDescription{
+					Fields: []pgproto3.FieldDescription{
+						{
+							TableAttributeNumber: 1,
+							Name:                 []byte("id"),
+							DataTypeOID:          23,
+							DataTypeSize:         4,
+							TypeModifier:         -1,
+							Format:               1,
+						},
+					},
+				},
+				&pgproto3.DataRow{
+					Values: [][]byte{
+						func() []byte {
+							res := make([]byte, 4)
+							binary.BigEndian.PutUint32(res, 1)
+							return res
+						}(),
+					},
+				},
+				&pgproto3.CommandComplete{
+					CommandTag: []byte("INSERT 0 1"),
 				},
 				&pgproto3.ReadyForQuery{
 					TxStatus: 73, /*txidle*/
