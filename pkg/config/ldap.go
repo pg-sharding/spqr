67cf554ddb5fa8da1da1bc4e9c1fdc11ab57401b
NitroLiine <sergey.and.natsha@yandex.ru>
2024-01-23 20:22:50 +0500

    add more configs | add example ldap server

--- pkg/config/ldap.go
+++ pkg/config/ldap.go
@@ -4,8 +4,10 @@ type LDAPCfg struct {
 	LdapServer          string `json:"ldapserver" yaml:"ldapserver" toml:"ldapserver"`
 	LdapPort            int    `json:"ldapport" yaml:"ldapport" toml:"ldapport"`
 	LdapScheme          string `json:"ldapscheme" yaml:"ldapscheme" toml:"ldapscheme"`
+	LdapTLS             bool   `json:"ldaptls" yaml:"ldaptls" toml:"ldaptls"`
 	LdapBaseDn          string `json:"ldapbasedn" yaml:"ldapbasedn" toml:"ldapbasedn"`
 	LdapBindDn          string `json:"ldapbinddn" yaml:"ldapbinddn" toml:"ldapbinddn"`
 	LdapBindPasswd      string `json:"ldapbindpasswd" yaml:"ldapbindpasswd" toml:"ldapbindpasswd"`
 	LdapSearchAttribute string `json:"ldapsearchattribute" yaml:"ldapsearchattribute" toml:"ldapsearchattribute"`
+	LdapSearchFilter    string `json:"ldapsearchfilter" yaml:"ldapsearchfilter" toml:"ldapsearchfilter"`
 }
