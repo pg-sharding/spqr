ed28a0b8f5b9ada3748d11d97e198343f1dd91ac
Yury Frolov <yura200253@gmail.com>
2024-02-07 18:59:46 +0500

    Changed & disabled router/routing_hint regression test

--- test/regress/tests/router/sql/routing_hint.sql
+++ test/regress/tests/router/sql/routing_hint.sql
@@ -1,12 +1,15 @@
 \c spqr-console
 
-CREATE SHARDING RULE t1 COLUMNS id;
-CREATE KEY RANGE krid1 FROM 1 ROUTE TO sh1;
-CREATE KEY RANGE krid2 FROM 11 ROUTE TO sh2;
+CREATE DISTRIBUTION ds1 COLUMN TYPES integer;
+CREATE SHARDING RULE t1 COLUMNS id FOR DISTRIBUTION ds1;
+CREATE KEY RANGE krid1 FROM 1 ROUTE TO sh1 FOR DISTRIBUTION ds1;
+CREATE KEY RANGE krid2 FROM 11 ROUTE TO sh2 FOR DISTRIBUTION ds1;
+ALTER DISTRIBUTION ds1 ATTACH RELATION test COLUMNS id;
 
 \c regress
 
 CREATE TABLE test(id int, age int);
+-- TODO: specify distribution as well as sharding_key
 INSERT INTO test(id, age) VALUES (10, 16) /*__spqr__sharding_key: 30*/;
 INSERT INTO test(id, age) VALUES (10, 16) /*__spqr__sharding_key: 3000*/;
 
