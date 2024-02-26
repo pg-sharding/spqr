095f2104db836d07b3add420ba9a80ff8b8042df
Yury Frolov <yura200253@gmail.com>
2024-02-26 15:14:16 +0500

    Added tests & fixed spqrql

--- pkg/decode/spqrql.go
+++ pkg/decode/spqrql.go
@@ -8,21 +8,17 @@ import (
 )
 
 // KeyRange returns query to create given key range
-// TODO unit tests
 func KeyRange(krg *protos.KeyRangeInfo) string {
 	/* TODO: composite key support */
-
 	return fmt.Sprintf("CREATE KEY RANGE %s FROM %s ROUTE TO %s FOR DISTRIBUTION %s;", krg.Krid, krg.KeyRange.LowerBound, krg.ShardId, krg.DistributionId)
 }
 
 // Distribution returns query to create given distribution
-// TODO unit tests
 func Distribution(ds *protos.Distribution) string {
 	return fmt.Sprintf("CREATE DISTRIBUTION %s COLUMN TYPES %s;", ds.Id, strings.Join(ds.ColumnTypes, ", "))
 }
 
 // DistributedRelation return query to attach relation to distribution
-// TODO unit tests
 func DistributedRelation(rel *protos.DistributedRelation, ds string) string {
 	elems := make([]string, len(rel.DistributionKey))
 	for j, el := range rel.DistributionKey {
