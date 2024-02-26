6c1d69537e3e753b132b24346ffb55e52878337a
Yury Frolov <yura200253@gmail.com>
2024-02-26 14:53:18 +0500

    Fix & refactor spqrdump

--- pkg/decode/spqrql.go
+++ pkg/decode/spqrql.go
@@ -2,22 +2,31 @@ package decode
 
 import (
 	"fmt"
+	"strings"
 
 	protos "github.com/pg-sharding/spqr/pkg/protos"
 )
 
-// TODO : unit tests
-func DecodeRule(rule *protos.ShardingRule) string {
+// KeyRange returns query to create given key range
+// TODO unit tests
+func KeyRange(krg *protos.KeyRangeInfo) string {
 	/* TODO: composite key support */
-	if rule.TableName != "" {
-		return fmt.Sprintf("CREATE SHARDING RULE %s TABLE %s COLUMN %s", rule.Id, rule.TableName, rule.ShardingRuleEntry[0].Column)
-	}
-	return fmt.Sprintf("CREATE SHARDING RULE %s COLUMN %s", rule.Id, rule.ShardingRuleEntry[0].Column)
+
+	return fmt.Sprintf("CREATE KEY RANGE %s FROM %s ROUTE TO %s FOR DSITRIBUTION %s;", krg.Krid, krg.KeyRange.LowerBound, krg.ShardId, krg.DistributionId)
 }
 
-// TODO : unit tests
-func DecodeKeyRange(krg *protos.KeyRangeInfo) string {
-	/* TODO: composite key support */
+// Distribution returns query to create given distribution
+// TODO unit tests
+func Distribution(ds *protos.Distribution) string {
+	return fmt.Sprintf("CREATE DISTRIBUTION %s COLUMN TYPES %s;", ds.Id, strings.Join(ds.ColumnTypes, ", "))
+}
 
-	return fmt.Sprintf("CREATE KEY RANGE %s FROM %s TO %s ROUTE TO %s", krg.Krid, krg.KeyRange.LowerBound, krg.KeyRange.UpperBound, krg.ShardId)
+// DistributedRelation return query to attach relation to distribution
+// TODO unit tests
+func DistributedRelation(rel *protos.DistributedRelation, ds string) string {
+	elems := make([]string, len(rel.DistributionKey))
+	for j, el := range rel.DistributionKey {
+		elems[j] = fmt.Sprintf("%s HASH FUNCTION %s", el.Column, el.HashFunction)
+	}
+	return fmt.Sprintf("ALTER DISTRIBUTION %s ATTACH RELATTION %s DISTRIBUTION KEY %s;", ds, rel.Name, strings.Join(elems, ", "))
 }
