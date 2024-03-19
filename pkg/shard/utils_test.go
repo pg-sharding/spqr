1b3bc9a927b16474cf4b8c82b6636a6f229b6bc0
IakimovNutria <71931538+IakimovNutria@users.noreply.github.com>
2024-03-19 20:41:42 +0500

    use mock/shard

--- pkg/shard/utils_test.go
+++ pkg/shard/utils_test.go
@@ -1,6 +1,9 @@
-package shard
+package shard_test
 
 import (
+	"github.com/golang/mock/gomock"
+	mocksh "github.com/pg-sharding/spqr/pkg/mock/shard"
+	"github.com/pg-sharding/spqr/pkg/shard"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -8,12 +11,23 @@ import (
 func TestShardIDs(t *testing.T) {
 	assert := assert.New(t)
 
-	emptyShards := []Shard{}
-	assert.Equal([]uint{}, ShardIDs(emptyShards), "ShardIDs should return an empty slice for no shards")
+	ctrl := gomock.NewController(t)
 
-	oneShard := []Shard{&mockShard{id: 1}}
-	assert.Equal([]uint{1}, ShardIDs(oneShard), "ShardIDs should return a slice with one ID for one shard")
+	sh1 := mocksh.NewMockShard(ctrl)
+	sh1.EXPECT().ID().Return(uint(1)).AnyTimes()
 
-	multipleShards := []Shard{&mockShard{id: 1}, &mockShard{id: 2}, &mockShard{id: 3}}
-	assert.Equal([]uint{1, 2, 3}, ShardIDs(multipleShards), "ShardIDs should return a slice with multiple IDs for multiple shards")
+	sh2 := mocksh.NewMockShard(ctrl)
+	sh2.EXPECT().ID().Return(uint(2)).AnyTimes()
+
+	sh3 := mocksh.NewMockShard(ctrl)
+	sh3.EXPECT().ID().Return(uint(3)).AnyTimes()
+
+	var emptyShards []shard.Shard
+	assert.Equal([]uint{}, shard.ShardIDs(emptyShards), "ShardIDs should return an empty slice for no shards")
+
+	oneShard := []shard.Shard{sh1}
+	assert.Equal([]uint{1}, shard.ShardIDs(oneShard), "ShardIDs should return a slice with one ID for one shard")
+
+	multipleShards := []shard.Shard{sh1, sh2, sh3}
+	assert.Equal([]uint{1, 2, 3}, shard.ShardIDs(multipleShards), "ShardIDs should return a slice with multiple IDs for multiple shards")
 }
