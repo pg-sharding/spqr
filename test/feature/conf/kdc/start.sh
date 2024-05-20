b6a87b74ecc565e3e63463a9cc3c69764f7f5d18
NitroLine <sergey.and.natasha@yandex.ru>
2024-05-20 16:08:30 +0500

    fix start.sh

--- test/feature/conf/kdc/start.sh
+++ test/feature/conf/kdc/start.sh
@@ -5,6 +5,6 @@ set -e
 # The /keytab directory is volume mounted on both kdc and cockroach. kdc
 # can create the keytab with kadmin.local here and it is then useable
 # by cockroach.
-kadmin.local -q "ktadd -k /keytab/tester.keytab postgres/gss_cockroach_1.gss_default@MY.EX"
+kadmin.local -q "ktadd -k /keytab/tester.keytab postgres/gss_spqr_1.gss_spqr@MY.EX"
 
 krb5kdc -n
