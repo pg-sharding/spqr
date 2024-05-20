0d830b0f2f69bcde1a1819fafc9d001ac0df2cc6
NitroLine <sergey.and.natasha@yandex.ru>
2024-05-21 01:11:17 +0500

    change start.sh

--- test/feature/conf/kdc/start.sh
+++ test/feature/conf/kdc/start.sh
@@ -3,6 +3,6 @@
 set -e
 
 # The /keytab directory is volume mounted on both kdc and spqr
-kadmin.local -q "ktadd -k /keytab/tester.keytab postgres/gss_spqr_1.gss_spqr@MY.EX"
+kadmin.local -q "ktadd -k /keytab/tester.keytab postgres/localhost@MY.EX"
 
 krb5kdc -n
