b1c2038db8512c5ee544a1d9351ffb06af235fcb
NitroLine <sergey.and.natasha@yandex.ru>
2024-05-20 22:53:14 +0500

    add shard3

--- test/feature/conf/kdc/start.sh
+++ test/feature/conf/kdc/start.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-# The /keytab directory is volume mounted on both kdc and cockroach. kdc
-# can create the keytab with kadmin.local here and it is then useable
-# by cockroach.
+# The /keytab directory is volume mounted on both kdc and spqr
 kadmin.local -q "ktadd -k /keytab/tester.keytab postgres/gss_spqr_1.gss_spqr@MY.EX"
 
 krb5kdc -n
