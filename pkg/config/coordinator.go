68f1fb0e9dd12970c8072a7490d22df735b2c83a
denchick <denchick@yandex-team.ru>
2024-12-28 00:03:36 +0100

    watch routers iteration timeout

--- pkg/config/coordinator.go
+++ pkg/config/coordinator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"gopkg.in/yaml.v2"
@@ -13,16 +14,17 @@ import (
 var cfgCoordinator Coordinator
 
 type Coordinator struct {
-	LogLevel             string     `json:"log_level" toml:"log_level" yaml:"log_level"`
-	QdbAddr              string     `json:"qdb_addr" toml:"qdb_addr" yaml:"qdb_addr"`
-	CoordinatorPort      string     `json:"coordinator_port" toml:"coordinator_port" yaml:"coordinator_port"`
-	GrpcApiPort          string     `json:"grpc_api_port" toml:"grpc_api_port" yaml:"grpc_api_port"`
-	Host                 string     `json:"host" toml:"host" yaml:"host"`
-	Auth                 *AuthCfg   `json:"auth" toml:"auth" yaml:"auth"`
-	FrontendTLS          *TLSConfig `json:"frontend_tls" yaml:"frontend_tls" toml:"frontend_tls"`
-	ShardDataCfg         string     `json:"shard_data" toml:"shard_data" yaml:"shard_data"`
-	UseSystemdNotifier   bool       `json:"use_systemd_notifier" toml:"use_systemd_notifier" yaml:"use_systemd_notifier"`
-	SystemdNotifierDebug bool       `json:"systemd_notifier_debug" toml:"systemd_notifier_debug" yaml:"systemd_notifier_debug"`
+	LogLevel             string        `json:"log_level" toml:"log_level" yaml:"log_level"`
+	QdbAddr              string        `json:"qdb_addr" toml:"qdb_addr" yaml:"qdb_addr"`
+	CoordinatorPort      string        `json:"coordinator_port" toml:"coordinator_port" yaml:"coordinator_port"`
+	GrpcApiPort          string        `json:"grpc_api_port" toml:"grpc_api_port" yaml:"grpc_api_port"`
+	Host                 string        `json:"host" toml:"host" yaml:"host"`
+	Auth                 *AuthCfg      `json:"auth" toml:"auth" yaml:"auth"`
+	FrontendTLS          *TLSConfig    `json:"frontend_tls" yaml:"frontend_tls" toml:"frontend_tls"`
+	ShardDataCfg         string        `json:"shard_data" toml:"shard_data" yaml:"shard_data"`
+	UseSystemdNotifier   bool          `json:"use_systemd_notifier" toml:"use_systemd_notifier" yaml:"use_systemd_notifier"`
+	SystemdNotifierDebug bool          `json:"systemd_notifier_debug" toml:"systemd_notifier_debug" yaml:"systemd_notifier_debug"`
+	IterationTimeout     time.Duration `json:"iteration_timeout" toml:"iteration_timeout" yaml:"iteration_timeout"`
 }
 
 // LoadCoordinatorCfg loads the coordinator configuration from the specified file path.
