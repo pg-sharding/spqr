06d40b311b467956502d2499611c93d74b9c58df
denchick <denchick@yandex-team.ru>
2025-01-03 12:42:45 +0100

    drop old shards.go content

--- pkg/models/topology/shards.go
+++ pkg/models/topology/shards.go
@@ -1,22 +1,3 @@
 package topology
 
-type Shard struct {
-	ID    string
-	Hosts []string
-}
 
-// NewShard creates a new Shard instance with the specified ID and hosts.
-// It returns a pointer to the created Shard.
-//
-// Parameters:
-//   - ID: The ID of the shard.
-//   - hosts: The list of hosts that the shard is distributed across.
-//
-// Returns:
-//   - *Shard: A pointer to the created Shard.
-func NewShard(ID string, hosts []string) *Shard {
-	return &Shard{
-		ID:    ID,
-		Hosts: hosts,
-	}
-}
