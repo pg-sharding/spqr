9b5b69aa1815e2e08c61af167ffd63343217bc35
reshke <reshke@double.cloud>
2025-01-11 10:01:23 +0000

    correct schedule

--- script/regress_pooler_local.sh
+++ script/regress_pooler_local.sh
@@ -5,7 +5,7 @@ set -ex
 for name in `ls -1 test/regress/tests/pooler/sql/`;
 do
 	echo $name start
-	cat test/regress/tests/pooler/sql/$name |  psql "host=localhost port=6432 dbname=db1" --echo-all --quiet # > test/regress/tests/pooler/expected/$(basename $name .sql).out 2>&1;
+	cat test/regress/tests/pooler/sql/$name |  psql "host=localhost port=6432 dbname=db1" --echo-all --quiet  > test/regress/tests/pooler/expected/$(basename $name .sql).out 2>&1;
 	RESULT=$?
 	if [ ! $RESULT -eq 0 ]; then 
 		exit 1
