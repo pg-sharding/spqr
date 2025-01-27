31f55bc9c6244270e4b95c68711a0d676bff4cc9
Yury Frolov <yura200253@gmail.com>
2025-01-27 14:12:47 +0500

    FIx false negative test runs

--- test/regress/run_tests_CI.sh
+++ test/regress/run_tests_CI.sh
@@ -2,15 +2,15 @@
 set -ex
 
 for UBUNTU_VERSION in focal jammy noble; do
-    make POSTGRES_VERSION=12 codename=$UBUNTU_VERSION regress
-    make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION regress
-    make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION regress
-    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION regress
-    make POSTGRES_VERSION=16 codename=$UBUNTU_VERSION regress
-    make POSTGRES_VERSION=17 codename=$UBUNTU_VERSION regress
-    make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION image=regress-mdb-image regress
-    make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION image=regress-mdb-image regress
-    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION image=regress-mdb-image regress
-    make POSTGRES_VERSION=16 codename=$UBUNTU_VERSION image=regress-mdb-image regress
+    make POSTGRES_VERSION=12 codename=$UBUNTU_VERSION regress && \
+    make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION regress && \
+    make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION regress && \
+    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION regress && \
+    make POSTGRES_VERSION=16 codename=$UBUNTU_VERSION regress && \
+    make POSTGRES_VERSION=17 codename=$UBUNTU_VERSION regress && \
+    make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION image=regress-mdb-image regress && \
+    make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION image=regress-mdb-image regress && \
+    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION image=regress-mdb-image regress && \ 
+    make POSTGRES_VERSION=16 codename=$UBUNTU_VERSION image=regress-mdb-image regress && \
     make POSTGRES_VERSION=17 codename=$UBUNTU_VERSION image=regress-mdb-image regress
 done
