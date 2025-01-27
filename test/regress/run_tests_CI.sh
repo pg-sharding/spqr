9d74cb3d28a75efda388013f173a87a696090c68
Yury Frolov <yura200253@gmail.com>
2025-01-27 16:41:13 +0500

    WTF

--- test/regress/run_tests_CI.sh
+++ test/regress/run_tests_CI.sh
@@ -10,7 +10,7 @@ for UBUNTU_VERSION in focal jammy noble; do
     make POSTGRES_VERSION=17 codename=$UBUNTU_VERSION regress && \
     make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_13 regress && \
     make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_14 regress && \
-    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_15 regress && \ 
+    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_15 regress && \
     make POSTGRES_VERSION=16 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_16 regress && \
     make POSTGRES_VERSION=17 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_17 regress
 done
