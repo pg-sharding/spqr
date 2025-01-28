1ec53007814e29be17b4db19b941bb4aae54299f
Yury Frolov <yura200253@gmail.com>
2025-01-28 13:26:34 +0500

    Split regress tests in twain

--- test/regress/run_tests_CI_mdb.sh
+++ test/regress/run_tests_CI_mdb.sh
@@ -2,12 +2,6 @@
 set -xEeuo pipefail
 
 for UBUNTU_VERSION in focal jammy noble; do
-    make POSTGRES_VERSION=12 codename=$UBUNTU_VERSION regress && \
-    make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION regress && \
-    make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION regress && \
-    make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION regress && \
-    make POSTGRES_VERSION=16 codename=$UBUNTU_VERSION regress && \
-    make POSTGRES_VERSION=17 codename=$UBUNTU_VERSION regress && \
     make POSTGRES_VERSION=13 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_13 regress && \
     make POSTGRES_VERSION=14 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_14 regress && \
     make POSTGRES_VERSION=15 codename=$UBUNTU_VERSION image=regress-mdb-image mdb-branch=MDB_15 regress && \
