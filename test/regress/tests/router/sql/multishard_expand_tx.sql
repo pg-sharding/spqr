92f6be765dbbe64ee15b2f5fd1aa51ff6f38149c
reshke <reshke@double.cloud>
2025-02-14 12:05:58 +0000

    Expand transaction part2

---
+++ test/regress/tests/router/sql/multishard_expand_tx.sql
@@ -0,0 +1,52 @@
+\c spqr-console
+
+-- check that numeric type works
+CREATE DISTRIBUTION ds1 COLUMN TYPES integer;
+CREATE KEY RANGE FROM 0 ROUTE TO sh1 FOR DISTRIBUTION ds1;
+CREATE KEY RANGE FROM 100 ROUTE TO sh2 FOR DISTRIBUTION ds1;
+CREATE KEY RANGE FROM 200 ROUTE TO sh3 FOR DISTRIBUTION ds1;
+CREATE KEY RANGE FROM 300 ROUTE TO sh4 FOR DISTRIBUTION ds1;
+
+ALTER DISTRIBUTION ds1 ATTACH RELATION xxm_expd DISTRIBUTION KEY id;
+
+\c regress
+
+CREATE TABLE xxm_expd(id int, j INT);
+
+COPY xxm_expd (id, j) FROM STDIN;
+1	1
+2	2
+3	3
+4	4
+5	5
+50	50
+51	51
+100	100
+101	101
+199	199
+200	200
+201	201
+299	299
+300	300
+301	301
+399	399
+\.
+
+
+BEGIN;
+UPDATE xxm_expd SET j = j + 1 WHERE id = 1 /* __spqr__engine_v2: true */;
+UPDATE xxm_expd SET j = j + 1 WHERE id = 101 /* __spqr__engine_v2: true */;
+UPDATE xxm_expd SET j = j + 1 WHERE id = 299 /* __spqr__engine_v2: true */;
+UPDATE xxm_expd SET j = j + 1 WHERE id = 399 /* __spqr__engine_v2: true */;
+COMMIT;
+
+SELECT * FROM xxm_expd ORDER BY id;
+
+UPDATE xxm_expd SET id = -1 /* __spqr__engine_v2: true */;
+DELETE FROM xxm_expd /* __spqr__engine_v2: true */;
+
+DROP TABLE xxm_expd;
+
+\c spqr-console
+DROP DISTRIBUTION ALL CASCADE;
+DROP KEY RANGE ALL;
