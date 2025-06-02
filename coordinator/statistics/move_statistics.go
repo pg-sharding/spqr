244599c84d63a82be01676bddec51dd30a0617d0
Yury Frolov <yura200253@gmail.com>
2025-06-02 17:44:38 +0500

    Massive simplifying + some fix

--- coordinator/statistics/move_statistics.go
+++ coordinator/statistics/move_statistics.go
@@ -1,6 +1,7 @@
 package statistics
 
 import (
+	"sync"
 	"time"
 
 	"github.com/pg-sharding/spqr/pkg/models/spqrerror"
@@ -16,15 +17,16 @@ const (
 )
 
 type statisticsInt struct {
-	CurrentExecTimes     map[string]time.Duration
+	CurrentExecTimes     sync.Map
 	TotalTimes           map[string]*MoveStatisticsElem
 	CurrentMoveStartTime time.Time
 	TotalMoves           int
 	MoveInProgress       bool
+	totalTimesMu         sync.Mutex
 }
 
 var moveStatistics = statisticsInt{
-	CurrentExecTimes: make(map[string]time.Duration),
+	CurrentExecTimes: sync.Map{},
 	TotalTimes:       make(map[string]*MoveStatisticsElem),
 }
 
@@ -35,8 +37,10 @@ type MoveStatisticsElem struct {
 
 func RecordMoveStart(t time.Time) error {
 	spqrlog.Zero.Debug().Msg("move stats: record move start")
+	moveStatistics.totalTimesMu.Lock()
+	defer moveStatistics.totalTimesMu.Unlock()
 	moveStatistics.MoveInProgress = true
-	moveStatistics.CurrentExecTimes = make(map[string]time.Duration)
+	moveStatistics.CurrentExecTimes = sync.Map{}
 	moveStatistics.CurrentMoveStartTime = t
 	return nil
 }
@@ -46,44 +50,72 @@ func RecordMoveFinish(t time.Time) error {
 	if !moveStatistics.MoveInProgress {
 		return spqrerror.New(spqrerror.SPQR_UNEXPECTED, "unable to record move finish: there's no move in progress")
 	}
+	moveStatistics.totalTimesMu.Lock()
+	defer moveStatistics.totalTimesMu.Unlock()
 	moveStatistics.MoveInProgress = false
-	for stat, duration := range moveStatistics.CurrentExecTimes {
+	moveStatistics.CurrentExecTimes.Range(func(key, value any) bool {
+		stat := key.(string)
+		duration := value.(time.Duration)
 		if _, ok := moveStatistics.TotalTimes[stat]; !ok {
 			moveStatistics.TotalTimes[stat] = &MoveStatisticsElem{}
 		}
 		moveStatistics.TotalTimes[stat].SampleCount++
 		moveStatistics.TotalTimes[stat].TotalDuration += duration
-	}
+		return true
+	})
 	if _, ok := moveStatistics.TotalTimes[MoveStatsTotalTime]; !ok {
 		moveStatistics.TotalTimes[MoveStatsTotalTime] = &MoveStatisticsElem{}
 	}
 	moveStatistics.TotalTimes[MoveStatsTotalTime].SampleCount++
 	moveStatistics.TotalTimes[MoveStatsTotalTime].TotalDuration += t.Sub(moveStatistics.CurrentMoveStartTime)
 	moveStatistics.TotalMoves++
-	moveStatistics.CurrentExecTimes = make(map[string]time.Duration)
+	moveStatistics.CurrentExecTimes = sync.Map{}
 	return nil
 }
 
 func RecordQDBOperation(stat string, duration time.Duration) {
 	if moveStatistics.MoveInProgress {
-		moveStatistics.CurrentExecTimes[MoveStatsQDBPrefix+"."+stat] += duration
-		moveStatistics.CurrentExecTimes[MoveStatsQDBTotalTime] += duration
+		statName := MoveStatsQDBPrefix + "." + stat
+		curValue, ok := moveStatistics.CurrentExecTimes.Load(statName)
+		if ok {
+			moveStatistics.CurrentExecTimes.Store(statName, curValue.(time.Duration)+duration)
+		} else {
+			moveStatistics.CurrentExecTimes.Store(statName, duration)
+		}
+		curValue, ok = moveStatistics.CurrentExecTimes.Load(MoveStatsQDBTotalTime)
+		if ok {
+			moveStatistics.CurrentExecTimes.Store(MoveStatsQDBTotalTime, curValue.(time.Duration)+duration)
+		} else {
+			moveStatistics.CurrentExecTimes.Store(MoveStatsQDBTotalTime, duration)
+		}
 	}
 }
 
 func RecordRouterOperation(duration time.Duration) {
 	if moveStatistics.MoveInProgress {
-		moveStatistics.CurrentExecTimes[MoveStatsRouterTime] += duration
+		curValue, ok := moveStatistics.CurrentExecTimes.Load(MoveStatsRouterTime)
+		if ok {
+			moveStatistics.CurrentExecTimes.Store(MoveStatsRouterTime, curValue.(time.Duration)+duration)
+		} else {
+			moveStatistics.CurrentExecTimes.Store(MoveStatsRouterTime, duration)
+		}
 	}
 }
 
 func RecordShardOperation(duration time.Duration) {
 	if moveStatistics.MoveInProgress {
-		moveStatistics.CurrentExecTimes[MoveStatsShardTime] += duration
+		curValue, ok := moveStatistics.CurrentExecTimes.Load(MoveStatsShardTime)
+		if ok {
+			moveStatistics.CurrentExecTimes.Store(MoveStatsShardTime, curValue.(time.Duration)+duration)
+		} else {
+			moveStatistics.CurrentExecTimes.Store(MoveStatsShardTime, duration)
+		}
 	}
 }
 
 func GetMoveStats() map[string]time.Duration {
+	moveStatistics.totalTimesMu.Lock()
+	defer moveStatistics.totalTimesMu.Unlock()
 	res := make(map[string]time.Duration)
 	for k, v := range moveStatistics.TotalTimes {
 		res[k] = v.TotalDuration / time.Duration(v.SampleCount)
