62b939c26b6fae41a10e1abb22c6ea66aaf4ffef
reshke <reshke@double.cloud>
2025-07-04 23:14:35 +0500

    Sync reference relation data transfer utilities (#1320)
    * Sync reference relation data transfer utilities
    
    * Update meta.go

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -576,7 +576,11 @@ func ProcMetadataCommand(ctx context.Context, tstmt spqrparser.Statement, mgr En
 		}
 		return cli.MoveTaskGroup(ctx, taskGroup, colTypes)
 	case *spqrparser.SyncReferenceTables:
-		return cli.ReportError(fmt.Errorf("sync reference table cmd not implemented"))
+		/* TODO: fix RelationSelector logic */
+		if err := mgr.SyncReferenceRelations(ctx, []string{stmt.RelationSelector}, stmt.ShardID); err != nil {
+			return err
+		}
+		return cli.SyncReferenceRelations([]string{stmt.RelationSelector}, stmt.ShardID)
 	default:
 		return ErrUnknownCoordinatorCommand
 	}
