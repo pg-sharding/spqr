5a99793228b87ba4b5180007268213c4636cfa7d
reshke <reshke@double.cloud>
2025-07-07 13:50:11 +0000

    f

--- test/regress/tests/console/sql/show_shards.sql
+++ test/regress/tests/console/sql/show_shards.sql
@@ -4,9 +4,9 @@ DROP SHARD sh3;
 DROP SHARD sh4;
 
 CREATE SHARD sh1 WITH HOSTS spqr_shard_1:6432,spqr_shard_1_replica:6432;
-CREATE SHARD sh2 WITH HOSTS spqr_shard_1:6432,spqr_shard_1_replica:6432;
-CREATE SHARD sh3 WITH HOSTS spqr_shard_1:6432,spqr_shard_1_replica:6432;
-CREATE SHARD sh4 WITH HOSTS spqr_shard_1:6432,spqr_shard_1_replica:6432;
+CREATE SHARD sh2 WITH HOSTS spqr_shard_2:6432,spqr_shard_2_replica:6432;
+CREATE SHARD sh3 WITH HOSTS spqr_shard_3:6432,spqr_shard_3_replica:6432;
+CREATE SHARD sh4 WITH HOSTS spqr_shard_4:6432,spqr_shard_4_replica:6432;
 
 CREATE SHARD sh1 WITH HOSTS localhost:6432;
 
