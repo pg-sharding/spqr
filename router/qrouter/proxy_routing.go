bd7cf48cfe0f2b2a7e4737d31eb40ab395dfb2d4
reshke <reshke@double.cloud>
2025-07-10 17:10:50 +0500

    Refactor ModifyQuery (#1359)

--- router/qrouter/proxy_routing.go
+++ router/qrouter/proxy_routing.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
-	"strconv"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgproto3"
@@ -1845,7 +1844,7 @@ func (qr *ProxyQrouter) insertSequenceValue(ctx context.Context, ds *distributio
 		if err != nil {
 			return err
 		}
-		newQuery, err := qr.ModifyQuery(query, colName, nextval)
+		newQuery, err := planner.ModifyQuery(query, colName, nextval)
 		if err != nil {
 			return err
 		}
@@ -1854,42 +1853,4 @@ func (qr *ProxyQrouter) insertSequenceValue(ctx context.Context, ds *distributio
 	}
 	*qr.query = query
 	return nil
-}
-
-func (qr *ProxyQrouter) ModifyQuery(query string, colname string, nextval int64) (string, error) {
-	nextvalStr := strconv.FormatInt(nextval, 10)
-
-	// Find the position of the opening parenthesis for the column list
-	colsOpenInd := strings.Index(query, "(")
-	if colsOpenInd == -1 {
-		return "", fmt.Errorf("invalid query: missing column list")
-	}
-
-	// Find the position of the closing parenthesis for the column list
-	colsCloseInd := strings.Index(query[colsOpenInd:], ")")
-	if colsCloseInd == -1 {
-		return "", fmt.Errorf("invalid query: missing closing parenthesis in column list")
-	}
-	colsCloseInd += colsOpenInd
-
-	// Find the position of the opening parenthesis for the values list
-	valuesOpenInd := strings.Index(query[colsCloseInd:], "(")
-	if valuesOpenInd == -1 {
-		return "", fmt.Errorf("invalid query: missing values list")
-	}
-	valuesOpenInd += colsCloseInd
-
-	// Find the position of the closing parenthesis for the values list
-	valuesCloseInd := strings.Index(query[valuesOpenInd:], ")")
-	if valuesCloseInd == -1 {
-		return "", fmt.Errorf("invalid query: missing closing parenthesis in values list")
-	}
-	valuesCloseInd += valuesOpenInd
-
-	// Construct the modified query
-	newQuery := query[:colsOpenInd+1] + colname + ", " + query[colsOpenInd+1:colsCloseInd] +
-		query[colsCloseInd:valuesOpenInd+1] + nextvalStr + ", " + query[valuesOpenInd+1:valuesCloseInd] +
-		query[valuesCloseInd:]
-
-	return newQuery, nil
 }
