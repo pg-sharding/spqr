e90b0ba90eb523139dafa096bca4d2fae2f8545d
reshke <reshke@double.cloud>
2025-07-10 14:09:43 +0000

    f

--- router/frontend/frontend.go
+++ router/frontend/frontend.go
@@ -58,6 +58,7 @@ func ProcessMessage(qr qrouter.QueryRouter, rst relay.RelayStateMgr, msg pgproto
 			// copy interface
 			cpQ := *q
 			q = &cpQ
+			qr.SetQuery(&q.String)
 			_, err := rst.ProcQueryAdvancedTx(q.String, func() error {
 				rst.AddQuery(q)
 				return rst.ProcessMessageBuf(true, true)
@@ -120,6 +121,7 @@ func ProcessMessage(qr qrouter.QueryRouter, rst relay.RelayStateMgr, msg pgproto
 		// copy interface
 		cpQ := *q
 		q = &cpQ
+		qr.SetQuery(&q.String)
 		_, err := rst.ProcQueryAdvancedTx(q.String, func() error {
 			rst.AddQuery(q)
 			// this call completes relay, sends RFQ
