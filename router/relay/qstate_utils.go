ed3341bca787b0ff73e56fd828ecd8bcfd6d03f4
reshke <reshke@double.cloud>
2025-07-17 12:20:41 +0000

    f

--- router/relay/qstate_utils.go
+++ router/relay/qstate_utils.go
@@ -5,7 +5,7 @@ import "strings"
 func virtualParamTransformName(name string) string {
 	retName := name
 	if strings.HasPrefix(retName, "__spqr__.") {
-		retName = "__spqr__" + strings.Trim(retName, "__spqr__.")
+		retName = "__spqr__" + strings.TrimPrefix(retName, "__spqr__.")
 	}
 
 	return retName
