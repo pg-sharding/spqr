214bd3880d92d2877b2443f445027dadc63c1348
reshke <reshke@double.cloud>
2025-07-18 10:35:18 +0500

    Unify slice describe portal logic (#1405)
    * Unify slice describe portal logic

---
+++ router/relay/relay_utils.go
@@ -0,0 +1,84 @@
+package relay
+
+import (
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgproto3"
+	"github.com/pg-sharding/spqr/pkg/models/spqrerror"
+	"github.com/pg-sharding/spqr/router/server"
+)
+
+func sliceDescribePortal(serv server.Server, portalDesc *pgproto3.Describe, bind *pgproto3.Bind) (*PortalDesc, error) {
+
+	shards := serv.Datashards()
+	if len(shards) == 0 {
+		return nil, spqrerror.New(spqrerror.SPQR_NO_DATASHARD, "No active shards")
+	}
+
+	shard := shards[0]
+	shardId := shard.ID()
+	shkey := shard.SHKey()
+
+	if err := serv.SendShard(bind, shkey); err != nil {
+		return nil, err
+	}
+
+	if err := serv.SendShard(portalDesc, shkey); err != nil {
+		return nil, err
+	}
+
+	if err := serv.SendShard(portalClose, shkey); err != nil {
+		return nil, err
+	}
+
+	if err := serv.SendShard(pgsync, shkey); err != nil {
+		return nil, err
+	}
+
+	rd := &PortalDesc{}
+	var saveCloseComplete *pgproto3.CloseComplete
+
+recvLoop:
+	for {
+		// https://www.postgresql.org/docs/current/protocol-flow.html
+
+		msg, _, err := serv.ReceiveShard(shardId)
+		if err != nil {
+			return nil, err
+		}
+		switch q := msg.(type) {
+		case *pgproto3.BindComplete:
+			// that's ok
+		case *pgproto3.ReadyForQuery:
+			break recvLoop
+		case *pgproto3.ErrorResponse:
+			return nil, fmt.Errorf("error describing slice portal: \"%s\"", q.Message)
+		case *pgproto3.NoData:
+			rd.nodata = pgNoData
+		case *pgproto3.CloseComplete:
+			saveCloseComplete = q
+
+		case *pgproto3.RowDescription:
+			// copy
+			rd.rd = &pgproto3.RowDescription{}
+
+			rd.rd.Fields = make([]pgproto3.FieldDescription, len(q.Fields))
+
+			for i := range len(q.Fields) {
+				s := make([]byte, len(q.Fields[i].Name))
+				copy(s, q.Fields[i].Name)
+
+				rd.rd.Fields[i] = q.Fields[i]
+				rd.rd.Fields[i].Name = s
+			}
+		default:
+			return nil, fmt.Errorf("received unexpected message type %T", msg)
+		}
+	}
+
+	if saveCloseComplete == nil {
+		return nil, fmt.Errorf("portal was not closed after describe")
+	}
+
+	return rd, nil
+}
