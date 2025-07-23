f294a0c1393699a3d28fb059e4042c663df867d4
reshke <reshke@double.cloud>
2025-07-23 08:45:47 +0000

    Fixes

--- test/xproto/proto_test.go
+++ test/xproto/proto_test.go
@@ -1475,6 +1475,117 @@ func TestPrepStmtSimpleProtoViolation(t *testing.T) {
 	}
 }
 
+func TestTxBlockDescribePortalXproto(t *testing.T) {
+	conn, err := getC()
+	if err != nil {
+		assert.NoError(t, err, "startup failed")
+		return
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	frontend := pgproto3.NewFrontend(conn, conn)
+	frontend.Send(&pgproto3.StartupMessage{
+		ProtocolVersion: 196608,
+		Parameters:      getConnectionParams(),
+	})
+	if err := frontend.Flush(); err != nil {
+		assert.NoError(t, err, "startup failed")
+	}
+
+	if err := waitRFQ(frontend); err != nil {
+		assert.NoError(t, err, "startup failed")
+		return
+	}
+
+	for gr, msgroup := range []MessageGroup{
+		{
+			Request: []pgproto3.FrontendMessage{
+
+				&pgproto3.Parse{
+					Name:  "xproto_tx_block_b",
+					Query: "BEGIN;",
+				},
+
+				&pgproto3.Parse{
+					Name:  "xproto_tx_block_r",
+					Query: "ROLLBACK;",
+				},
+				&pgproto3.Bind{
+					PreparedStatement: "xproto_tx_block_b",
+				},
+				&pgproto3.Describe{
+					Name:       "",
+					ObjectType: 'P',
+				},
+				&pgproto3.Execute{},
+				&pgproto3.Bind{
+					PreparedStatement: "xproto_tx_block_r",
+				},
+				&pgproto3.Describe{
+					Name:       "",
+					ObjectType: 'P',
+				},
+				&pgproto3.Execute{},
+				&pgproto3.Sync{},
+			},
+			Response: []pgproto3.BackendMessage{
+				&pgproto3.ParseComplete{},
+				&pgproto3.ParseComplete{},
+				&pgproto3.BindComplete{},
+				&pgproto3.NoData{},
+				&pgproto3.CommandComplete{
+					CommandTag: []byte("BEGIN"),
+				},
+				&pgproto3.BindComplete{},
+				&pgproto3.NoData{},
+				&pgproto3.CommandComplete{
+					CommandTag: []byte("ROLLBACK"),
+				},
+				&pgproto3.ReadyForQuery{
+					TxStatus: byte(txstatus.TXIDLE),
+				},
+			},
+		},
+	} {
+		for _, msg := range msgroup.Request {
+			frontend.Send(msg)
+		}
+		_ = frontend.Flush()
+		backendFinished := false
+		for i, msg := range msgroup.Response {
+			if backendFinished {
+				break
+			}
+			retMsg, err := frontend.Receive()
+			assert.NoError(t, err)
+			switch retMsgType := retMsg.(type) {
+			case *pgproto3.ErrorResponse:
+				retMsgType.Routine = ""
+				retMsgType.Line = 0
+				retMsgType.File = ""
+				retMsgType.SeverityUnlocalized = ""
+			case *pgproto3.RowDescription:
+				for i := range retMsgType.Fields {
+					// We don't want to check table OID
+					retMsgType.Fields[i].TableOID = 0
+				}
+			case *pgproto3.ReadyForQuery:
+				switch msg.(type) {
+				case *pgproto3.ReadyForQuery:
+					break
+				default:
+					backendFinished = true
+				}
+			default:
+				break
+			}
+			assert.Equal(t, msg, retMsg, "group %v iter %d", gr, i)
+		}
+	}
+}
+
 func TestPrepStmtMultishardXproto(t *testing.T) {
 	conn, err := getC()
 	if err != nil {
@@ -1554,8 +1665,6 @@ func TestPrepStmtMultishardXproto(t *testing.T) {
 			},
 		},
 
-		/* XXX: todo - fix message ordering in describe portal */
-
 		{
 			Request: []pgproto3.FrontendMessage{
 
@@ -1576,26 +1685,26 @@ func TestPrepStmtMultishardXproto(t *testing.T) {
 				&pgproto3.Bind{
 					PreparedStatement: "xproto_ddl_multishard_ref_rel_b",
 				},
-				// &pgproto3.Describe{
-				// 	Name:       "",
-				// 	ObjectType: 'P',
-				// },
+				&pgproto3.Describe{
+					Name:       "",
+					ObjectType: 'P',
+				},
 				&pgproto3.Execute{},
 				&pgproto3.Bind{
 					PreparedStatement: "xproto_ddl_multishard_ref_rel_i",
 				},
-				// &pgproto3.Describe{
-				// 	Name:       "",
-				// 	ObjectType: 'P',
-				// },
+				&pgproto3.Describe{
+					Name:       "",
+					ObjectType: 'P',
+				},
 				&pgproto3.Execute{},
 				&pgproto3.Bind{
 					PreparedStatement: "xproto_ddl_multishard_ref_rel_r",
 				},
-				// &pgproto3.Describe{
-				// 	Name:       "",
-				// 	ObjectType: 'P',
-				// },
+				&pgproto3.Describe{
+					Name:       "",
+					ObjectType: 'P',
+				},
 				&pgproto3.Execute{},
 				&pgproto3.Sync{},
 			},
@@ -1604,17 +1713,17 @@ func TestPrepStmtMultishardXproto(t *testing.T) {
 				&pgproto3.ParseComplete{},
 				&pgproto3.ParseComplete{},
 				&pgproto3.BindComplete{},
-				// &pgproto3.NoData{},
+				&pgproto3.NoData{},
 				&pgproto3.CommandComplete{
 					CommandTag: []byte("BEGIN"),
 				},
 				&pgproto3.BindComplete{},
-				// &pgproto3.NoData{},
+				&pgproto3.NoData{},
 				&pgproto3.CommandComplete{
 					CommandTag: []byte("INSERT 0 1"),
 				},
 				&pgproto3.BindComplete{},
-				// &pgproto3.NoData{},
+				&pgproto3.NoData{},
 				&pgproto3.CommandComplete{
 					CommandTag: []byte("ROLLBACK"),
 				},
