9368d9967aeb43794d1127493b03df70871f89ac
denchick <3149929+Denchick@users.noreply.github.com>
2025-08-07 11:13:27 +0200

    proxy errors from shard test (#1496)
    * proxy errors from shard test
    
    * fixes

--- test/regress/tests/router/sql/error2.sql
+++ test/regress/tests/router/sql/error2.sql
@@ -2,35 +2,45 @@
 
 CREATE DISTRIBUTION ds1 COLUMN TYPES INTEGER;
 
-ALTER DISTRIBUTION ds1 ATTACH RELATION tt DISTRIBUTION KEY i;
+ALTER DISTRIBUTION ds1 ATTACH RELATION tt DISTRIBUTION KEY id;
 
-CREATE KEY RANGE krid4 FROM 100 ROUTE TO sh2 FOR DISTRIBUTION ds1;
-CREATE KEY RANGE krid3 FROM 70 ROUTE TO sh2 FOR DISTRIBUTION ds1;
-CREATE KEY RANGE krid2 FROM 30 ROUTE TO sh2 FOR DISTRIBUTION ds1;
 CREATE KEY RANGE krid1 FROM 0 ROUTE TO sh1 FOR DISTRIBUTION ds1;
+CREATE KEY RANGE krid2 FROM 50 ROUTE TO sh2 FOR DISTRIBUTION ds1;
 
 \c regress
 
-CREATE TABLE tt(i INT) /*  */;
+-- test table does not exist
+INSERT INTO tt (id, data) VALUES (2, 'valid');
 
-SET __spqr__engine_v2 TO false;
+-- create table after error
+CREATE TABLE tt(id INT PRIMARY KEY, data TEXT);
 
+-- insert initial data
+INSERT INTO tt (id, data) VALUES (1, 'first');
+INSERT INTO tt (id, data) VALUES (51, 'second');
+
+-- test unique constraint violation on shard
 BEGIN;
-
-INSERT INTO tt (i) VALUES(1);
-
-DROP TABLE tt;
-
-INSERT INTO tt (i) VALUES(1);
-INSERT INTO tt (i) VALUES(1);
-INSERT INTO tt (i) VALUES(1);
-INSERT INTO tt (i) VALUES(1);
-
+INSERT INTO tt (id, data) VALUES (2, 'valid');
+-- this should cause a unique constraint violation error from shard
+INSERT INTO tt (id, data) VALUES (1, 'duplicate');
+-- after error, transaction should be aborted - this should fail with SPQRU
+INSERT INTO tt (id, data) VALUES (3, 'should_fail');
 ROLLBACK;
 
--- should exists 
-SET __spqr__engine_v2 TO true;
-TABLE tt;
+-- test that table is still accessible after rollback
+SELECT COUNT(*) FROM tt;
+
+-- test cross-shard transaction with error
+BEGIN;
+INSERT INTO tt (id, data) VALUES (10, 'shard1');
+INSERT INTO tt (id, data) VALUES (60, 'shard2');
+-- violate constraint on different shard
+INSERT INTO tt (id, data) VALUES (51, 'duplicate_shard2');
+-- should fail with transaction aborted error
+SELECT * FROM tt WHERE id = 10;
+ROLLBACK;
+
 DROP TABLE tt;
 
 \c spqr-console
