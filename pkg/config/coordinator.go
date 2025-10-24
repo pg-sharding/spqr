76bf267758de5de8dee3243fca133576bd48c1f8
Yury Frolov <yura200253@gmail.com>
2025-10-24 13:49:47 +0500

    disable_triggers -> data-move_disable triggers

--- pkg/config/coordinator.go
+++ pkg/config/coordinator.go
@@ -32,7 +32,7 @@ type Coordinator struct {
 	RolesFile            string          `json:"roles_file" toml:"roles_file" yaml:"roles_file"`
 
 	EtcdMaxSendBytes        int  `json:"etcd_max_send_bytes" toml:"etcd_max_send_bytes" yaml:"etcd_max_send_bytes"`
-	DataMoveDisableTriggers bool `json:"disable_triggers" toml:"disable_triggers" yaml:"disable_triggers"`
+	DataMoveDisableTriggers bool `json:"data_move_disable_triggers" toml:"data_move_disable_triggers" yaml:"data_move_disable_triggers"`
 }
 
 // LoadCoordinatorCfg loads the coordinator configuration from the specified file path.
