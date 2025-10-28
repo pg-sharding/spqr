8e938de92e872ed127026084f4696b5bc1a0934c
reshke <reshke@double.cloud>
2025-10-28 12:56:51 +0000

    Enable virtual query processing tests

--- test/regress/tests/router/sql/virtual.sql
+++ test/regress/tests/router/sql/virtual.sql
@@ -5,6 +5,8 @@ SELECT 1, 2, 3;
 
 SELECT 'postgres is cool';
 
+SET __spqr__target_session_attrs to 'read-only';
+
 SELECT pg_is_in_recovery();
 
 SELECT __spqr__is_ready();
@@ -14,6 +16,11 @@ SELECT current_setting('transaction_read_only');
 SELECT pg_is_in_recovery(), 1, 'kek';
 
 SELECT CURRENT_USER;
+
+select pg_is_in_recovery(), not pg_is_in_recovery(), __spqr__is_ready(), 1, 'a';
+
+set __spqr__preferred_engine to v2;
+select __spqr__shards();
 
 --- XXX: support
 --- SELECT 1,2,3 UNION ALL SELECT 2,3,4;
