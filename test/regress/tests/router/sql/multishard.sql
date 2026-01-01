d8a9d52a06bd57224702c62fafa4ff708f740c32
reshke <reshke@double.cloud>
2026-01-01 15:23:37 +0000

    f8

--- test/regress/tests/router/sql/multishard.sql
+++ test/regress/tests/router/sql/multishard.sql
@@ -50,9 +50,9 @@ SELECT id FROM xxmultish WHERE id = 0 OR id = 399;
 SELECT id FROM xxmultish WHERE id = 1 OR id = 299 OR id = 350;
 SELECT id FROM xxmultish WHERE id = 299 OR id = 350;
 
-SELECT id FROM xxmultish WHERE id = 201 UNION ALL SELECT * FROM xxmultish WHERE id = 199;
-SELECT id FROM xxmultish WHERE id = 401 UNION ALL SELECT * FROM xxmultish WHERE id = 99;
-SELECT id FROM xxmultish WHERE id = 401 EXCEPT ALL SELECT * FROM xxmultish WHERE id = 99;
+SELECT id FROM xxmultish WHERE id = 201 UNION ALL SELECT id FROM xxmultish WHERE id = 199;
+SELECT id FROM xxmultish WHERE id = 401 UNION ALL SELECT id FROM xxmultish WHERE id = 99;
+SELECT id FROM xxmultish WHERE id = 401 EXCEPT ALL SELECT id FROM xxmultish WHERE id = 99;
 
 WITH d AS (SELECT * FROM xxmultish WHERE id = 401 OR id = 0) TABLE d;
 
