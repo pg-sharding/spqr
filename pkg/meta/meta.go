74e4765ef4a995d7aa7b6cc4bca26d7b8adfaac1
Konstantin Kolesnikov <110341007+kla1mn@users.noreply.github.com>
2026-01-11 20:17:15 +0500

    Feat: MOVE KEY RANGE command  hint (#1810) (#1884)

--- pkg/meta/meta.go
+++ pkg/meta/meta.go
@@ -897,6 +897,7 @@ func ProcMetadataCommand(ctx context.Context, tstmt spqrparser.Statement, mgr En
 			Desc: engine.GetVPHeader("move key range"),
 			Raw: [][][]byte{
 				{fmt.Appendf(nil, "move key range %v to shard %v", move.Krid, move.ShardId)},
+				{[]byte("HINT: MOVE KEY RANGE only updates metadata. Use REDISTRIBUTE KEY RANGE to also migrate data.")},
 			},
 		}
 
