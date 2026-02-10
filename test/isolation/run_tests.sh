5b6aafd802aac51801577b2c8928a45047fd05ea
reshke <reshke@double.cloud>
2026-02-10 22:00:42 +0000

    fix

--- test/isolation/run_tests.sh
+++ test/isolation/run_tests.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -x
+
 export PGDATABASE=regress
 export PGUSER=regress
 export PGSSLMODE=allow
+
+while ! pg_isready -p 6432 -U regress -d regress -h regress_router
+do
+    echo "router still not repodning"
+    sleep 1
+done
+
+psql "host=regress_router port=6432 dbname=spqr-console user=regress" -c 'REGISTER ROUTER r1 ADDRESS "[regress_router]:7000"'
 
 /postgres/src/test/isolation/pg_isolation_regress \
     --inputdir /regress \
@@ -16,7 +26,5 @@ export PGSSLMODE=allow
     --debug \
     --schedule /regress/schedule
 
-
-if test -f /var/log/spqr-router.log; then cat /var/log/spqr-router.log; exit 1; fi
 
 if test -f /regress/regression.diffs; then cat /regress/regression.diffs; exit 1; fi
