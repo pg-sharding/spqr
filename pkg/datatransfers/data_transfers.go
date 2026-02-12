5b37a9aeacc5631c01253e8a14208493381b70b4
Yury Frolov <yura200253@gmail.com>
2026-02-12 19:49:43 +0500

    Fix

--- pkg/datatransfers/data_transfers.go
+++ pkg/datatransfers/data_transfers.go
@@ -646,7 +646,7 @@ func copyReferenceRelationData(ctx context.Context, from, to *pgx.Conn, fromId, 
 	dbName := fromShard.DB
 	fromHost := strings.Split(fromShard.Hosts[0], ":")[0]
 	hasher := murmur3.New64()
-	if _, err := hasher.Write(fmt.Appendf(nil, "%s_%s_%s", strings.Split(toShard.Hosts[0], ":")[0], dbName, fromHost)); err != nil {
+	if _, err := hasher.Write(fmt.Appendf(nil, "%s_%s_%s_%s_%s", toId, strings.Split(toShard.Hosts[0], ":")[0], dbName, fromId, fromHost)); err != nil {
 		return err
 	}
 	serverNameHash := hasher.Sum64()
