65de592ed80520ba73d27913dc9a1e712dabefe3
Alex Klepov <alexezm@yandex.ru>
2026-03-15 13:43:36 +0300

    move keyrange lock BL to meta layer step 2

--- coordinator/pkg/clustered_coord.go
+++ coordinator/pkg/clustered_coord.go
@@ -741,7 +741,7 @@ func (qc *ClusteredCoordinator) CreateKeyRange(ctx context.Context, keyRange *kr
 
 // TODO : unit tests
 func (qc *ClusteredCoordinator) LockKeyRange(ctx context.Context, keyRangeID string) (*kr.KeyRange, error) {
-	keyRange, err := meta.LockKeyRange(ctx, &qc.Coordinator, keyRangeID)
+	keyRange, err := qc.Coordinator.LockKeyRange(ctx, keyRangeID)
 	if err != nil {
 		return nil, err
 	}
