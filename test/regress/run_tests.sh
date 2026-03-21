9e635ee3ab527a1b71ef370c137c3054605469f9
Alex Klepov <alexezm@yandex.ru>
2026-03-21 17:22:07 +0300

    test regress error

--- test/regress/run_tests.sh
+++ test/regress/run_tests.sh
@@ -4,6 +4,8 @@ export PGDATABASE=regress
 export PGUSER=regress
 export PGSSLMODE=allow
 
+source ./regress_utils.sh 
+
 run_tests () {
     DIR=$1  # router
     HOST=$2 # regress_router
@@ -42,10 +44,14 @@ https://github.com/pg-sharding/spqr/tree/master/docs
 
 
 run_tests "console" "regress_coordinator" "7002"
-if test -f /regress/tests/console/regression.diffs; then mkdir /regress/tests/console_coordinator && mv /regress/tests/console/regression.diffs /regress/tests/console_coordinator/regression.diffs; fi
+
+save_diffs /regress/tests/console/
+
 insert_greeting "console"
+
 run_tests "console" "regress_router" "7432"
-if test -f /regress/tests/console/regression.diffs; then mkdir /regress/tests/console_router && mv /regress/tests/console/regression.diffs /regress/tests/console_router/regression.diffs; fi
+
+save_diffs /regress/tests/console/
 
 run_tests "router" "regress_router" "6432"
 run_tests "pooler" "regress_pooler" "6432"
@@ -53,13 +59,16 @@ run_tests "coordinator" "regress_coordinator" "7002"
 
 # these tests are to compare the results of the local and qdb coordinators
 run_tests "common" "regress_coordinator" "7002"
-if test -f /regress/tests/common/regression.diffs; then mkdir /regress/tests/common_coordinator && mv /regress/tests/common/regression.diffs /regress/tests/common_coordinator/regression.diffs; fi
+
+save_diffs /regress/tests/common/
+
 insert_greeting "common"
 run_tests "common" "regress_router" "7432"
-if test -f /regress/tests/common/regression.diffs; then mkdir /regress/tests/common_router && mv /regress/tests/common/regression.diffs /regress/tests/common_router/regression.diffs; fi
+
+save_diffs /regress/tests/common/
 
 # test if diffs are empty
-cat /regress/tests/**/regression.diffs > /regress/tests/combined.diffs 2>&-
+cat $ERR_OUTPUT_DIR/regression.diffs > /regress/tests/combined.diffs 2>&-
 
 if test -s /regress/tests/combined.diffs; then
     exit 1
