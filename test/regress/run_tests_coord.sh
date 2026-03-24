0a0736e8a7824b0f8684dc77d46de0616127df0f
Alex Klepov <alexezm@yandex.ru>
2026-03-24 09:39:28 +0300

    Rework regression test runner bash.
    Reuse handy functions from 4255f5dfe.
    
    Suggested-by: reshke <reshke@double.cloud>
    Reviewed-by: reshke <reshke@double.cloud>

--- test/regress/run_tests_coord.sh
+++ test/regress/run_tests_coord.sh
@@ -4,6 +4,8 @@ export PGDATABASE=regress
 export PGUSER=regress
 export PGSSLMODE=allow
 
+source ./regress_utils.sh 
+
 run_tests () {
     DIR=$1  # router
     HOST=$2 # regress_router
@@ -39,20 +41,6 @@ https://github.com/pg-sharding/spqr/tree/master/docs
         mv tmpfile $f
     done
 }
-
-ERR_OUTPUT_DIR=/tmp/regress_diffs
-
-save_diffs() {
-    mkdir -p $ERR_OUTPUT_DIR
-    
-    diff_files=$(find "$1" -name regression.diffs)
-    for diff_file in ${diff_files}; do
-        mv $diff_file $ERR_OUTPUT_DIR/$(basename $diff_file)
-    done
-    
-}
-
-
 
 echo "wait for services started"
 sleep 10
