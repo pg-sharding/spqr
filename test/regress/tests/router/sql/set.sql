b118755ede8be578b586c8e9338bfeb2b9fa2f3e
diphantxm <diphantxm@yandex-team.ru>
2026-03-26 12:51:42 +0300

    wip

---
+++ test/regress/tests/router/sql/set.sql
@@ -0,0 +1,35 @@
+\c spqr-console
+
+CREATE DISTRIBUTION ds1 COLUMN TYPES integer;
+
+CREATE KEY RANGE FROM 301 ROUTE TO sh4 FOR DISTRIBUTION ds1;
+CREATE KEY RANGE FROM 201 ROUTE TO sh3 FOR DISTRIBUTION ds1;
+
+\c regress
+
+SET application_name TO 'regress';
+SHOW application_name;
+
+BEGIN;
+SET LOCAL application_name TO 'regress_local';
+SHOW application_name;
+ROLLBACK;
+SHOW application_name;
+
+BEGIN;
+SET LOCAL application_name TO 'regress_local';
+SHOW application_name;
+COMMIT;
+SHOW application_name;
+
+BEGIN;
+SET application_name TO 'regress_tx';
+SHOW application_name;
+ROLLBACK;
+SHOW application_name;
+
+BEGIN;
+SET application_name TO 'regress_tx';
+SHOW application_name;
+COMMIT;
+SHOW application_name;
