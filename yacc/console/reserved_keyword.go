3caf2974f767934867feec0465604fa6494096ef
Ledatu <zavoronok.danila@gmail.com>
2026-04-08 22:43:10 +0300

    feat: add shard TLS parser and proto primitives

--- yacc/console/reserved_keyword.go
+++ yacc/console/reserved_keyword.go
@@ -105,4 +105,8 @@ var reservedWords = map[string]int{
 	"system":       SYSTEM,
 	"reload":       RELOAD,
 	"restart":      RESTART,
+	"sslmode":        SSLMODE,
+	"cert_file":      CERT_FILE,
+	"key_file":       KEY_FILE,
+	"root_cert_file": ROOT_CERT_FILE,
 }
