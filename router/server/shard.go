bc59909beb95fc46d3ce4ccb1e39f030a4854171
reshke <reshke@double.cloud>
2026-04-16 23:15:09 +0000

    d

--- router/server/shard.go
+++ router/server/shard.go
@@ -163,7 +163,7 @@ func (srv *ShardServer) SendShard(query pgproto3.FrontendMessage, shkey kr.Shard
 	return srv.Send(query)
 }
 
-func (m *ShardServer) PrefetchResult(shkey kr.ShardKey, syncCnt uint) error {
+func (m *ShardServer) PrefetchResult(_ kr.ShardKey, _ uint) error {
 	return nil
 }
 
