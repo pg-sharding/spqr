ccf5916c34a0d3b2fb7e92d4fd82e89429ce5472
reshke <reshke@double.cloud>
2026-04-21 08:57:02 +0000

    f

--- test/stress/run_tests.sh
+++ test/stress/run_tests.sh
@@ -25,8 +25,7 @@ pgbench --builtin select-only -c $NUM_CLIENTS -T $TEST_DURATION -n -h stress_rou
 echo "======== RUN AGAINST POSTGRES ========"
 pgbench --builtin select-only -c $NUM_CLIENTS -T $TEST_DURATION -n -h spqr_shard_1 -U $PGUSER $PGDATABASE
 
-
-psql -h spqr_shard_1 -U $PGUSER $PGDATABASE -f boot.sql
+psql -h stress_router -U $PGUSER $PGDATABASE -f boot.sql
 
 echo "======== RUN DEADLOCK TEST ========"
 pgbench --builtin select-only -f f.sql -c $NUM_CLIENTS -T $TEST_DURATION -n -h stress_router -U $PGUSER $PGDATABASE
