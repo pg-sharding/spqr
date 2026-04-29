f28138efe4526b9297d31162a4f0168997c98f92
reshke <reshke@double.cloud>
2026-04-29 08:40:49 +0500

    Switch errComplexQuery to use `spqrerror` infrastructure  (#2414)

--- router/rerrors/errors.go
+++ router/rerrors/errors.go
@@ -1,7 +1,11 @@
 package rerrors
 
-import "fmt"
+import (
+	"fmt"
 
-var ErrComplexQuery = fmt.Errorf("too complex query to route")
+	"github.com/pg-sharding/spqr/pkg/models/spqrerror"
+)
+
+var ErrComplexQuery = spqrerror.Newf(spqrerror.SPQR_NOT_IMPLEMENTED, "too complex query to route")
 var ErrExecutorSyncLost = fmt.Errorf("sync lost in execution phase")
 var ErrInformationSchemaCombinedQuery = fmt.Errorf("combined information schema and regular relation is not supported")
