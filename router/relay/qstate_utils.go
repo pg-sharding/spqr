eaaf8956ab3e3004c714777bfff2167295b81f61
reshke <reshke@double.cloud>
2026-05-03 11:58:52 +0000

    Small refactoring for `DispatchSlice`

--- router/relay/qstate_utils.go
+++ router/relay/qstate_utils.go
@@ -4,10 +4,10 @@ import (
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgproto3"
+	"github.com/pg-sharding/spqr/pkg/models/kr"
 	"github.com/pg-sharding/spqr/pkg/plan"
 	"github.com/pg-sharding/spqr/pkg/session"
 	"github.com/pg-sharding/spqr/router/client"
-	"github.com/pg-sharding/spqr/router/server"
 )
 
 func virtualParamTransformName(name string) string {
@@ -24,114 +24,77 @@ func DispatchSlice(qd *QueryDesc,
 
 	serv := cl.Server()
 
-	shkey := server.ServerShkeys(serv)
+	var et []kr.ShardKey
 
 	if p == nil {
-
-		if qd.simple {
-			if err := serv.Send(qd.Msg); err != nil {
-				return err
-			}
-		} else {
-
-			/* this message is actually bind */
-			if err := serv.Send(qd.Msg); err != nil {
-				return err
-			}
-
-			if err := serv.Send(qd.exec); err != nil {
-				return err
-			}
-
-			if err := serv.Send(pgsync); err != nil {
-				return err
-			}
+		for _, ds := range serv.Datashards() {
+			et = append(et, ds.SHKey())
 		}
-
 	} else {
-		et := p.ExecutionTargets()
+		et = p.ExecutionTargets()
 
 		if len(et) == 0 {
+			for _, ds := range serv.Datashards() {
+				et = append(et, ds.SHKey())
+			}
+		}
+	}
 
-			if qd.simple {
-				if err := serv.Send(qd.Msg); err != nil {
+	for _, targ := range et {
+
+		if qd.simple {
+
+			/*
+			* This is only execution path for non-top level slice
+			 */
+
+			if ovMsg := p.GetGangMemberMsg(targ); ovMsg != "" {
+				/* Uh, oh, this is very ugly hack */
+
+				if err := serv.SendShard(&pgproto3.Query{
+					String: ovMsg,
+				}, targ); err != nil {
 					return err
 				}
+
+				guc, err := cl.FindBoolGUC(session.SPQR_LINEARIZE_DISPATCH)
+				if err != nil {
+					return err
+				}
+
+				if guc.Get(cl) {
+					if err := serv.PrefetchResult(targ, 1); err != nil {
+						return err
+					}
+				}
+
 			} else {
 
-				/* this message is actually bind */
-				if err := serv.Send(qd.Msg); err != nil {
+				/* Assert for IsQuery here? */
+				if err := serv.SendShard(qd.Msg, targ); err != nil {
 					return err
 				}
 
-				if err := serv.Send(qd.exec); err != nil {
-					return err
-				}
-
-				if err := serv.Send(pgsync); err != nil {
-					return err
-				}
 			}
 
 		} else {
-			for _, targ := range et {
-
-				if qd.simple {
-
-					/*
-					* This is only execution patch for non-top level slice
-					 */
-
-					if ovMsg := p.GetGangMemberMsg(targ); ovMsg != "" {
-						/* Uh, oh, this is very ugly hack */
-
-						if err := serv.SendShard(&pgproto3.Query{
-							String: ovMsg,
-						}, targ); err != nil {
-							return err
-						}
-
-						guc, err := cl.FindBoolGUC(session.SPQR_LINEARIZE_DISPATCH)
-						if err != nil {
-							return err
-						}
-
-						if guc.Get(cl) {
-							if err := serv.PrefetchResult(targ, 1); err != nil {
-								return err
-							}
-						}
-
-					} else {
-
-						/* Assert for IsQuery here? */
-						if err := serv.SendShard(qd.Msg, targ); err != nil {
-							return err
-						}
-
-					}
-
-				} else {
-					/* this message is actually bind */
-					if err := serv.SendShard(qd.Msg, targ); err != nil {
-						return err
-					}
-
-					if err := serv.SendShard(qd.exec, targ); err != nil {
-						return err
-					}
-
-					if err := serv.SendShard(pgsync, targ); err != nil {
-						return err
-					}
-				}
+			/* this message is actually bind */
+			if err := serv.SendShard(qd.Msg, targ); err != nil {
+				return err
 			}
-			shkey = et
+
+			if err := serv.SendShard(qd.exec, targ); err != nil {
+				return err
+			}
+
+			if err := serv.SendShard(pgsync, targ); err != nil {
+				return err
+			}
 		}
 	}
 
 	if cl.ShowNoticeMsg() && replyCl {
-		_ = replyShardMatchesWithHosts(cl, serv, shkey)
+		_ = replyShardMatchesWithHosts(cl, serv, et)
 	}
 	return nil
 }
