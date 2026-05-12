df509cdb230b5ad53f8677d3ea39c264fde0c725
reshke <reshke@double.cloud>
2026-05-12 18:46:40 +0500

    Update f.sql

--- test/stress/f.sql
+++ test/stress/f.sql
@@ -9,7 +9,6 @@ COMMIT;
 -- with force auto-linearize
 BEGIN;
 SET __spqr__engine_v2 TO true;
-SET __spqr__linearize_dispatch TO false;
 
 INSERT INTO t (i, j) VALUES (1, 0), (200, 0), (300, 0) ON CONFLICT (i) DO UPDATE SET j = t.j + 1 /*__spqr__linearize_dispatch: ok */;
 
