254dd1a93291433f31197c55cbe00bd27e5c43ec
reshke <reshke@double.cloud>
2026-05-12 20:22:45 +0500

    Update deadlock stress test runner to exercise auto-linearize code (#2520)

--- test/stress/f.sql
+++ test/stress/f.sql
@@ -5,3 +5,11 @@ SET __spqr__linearize_dispatch TO true;
 INSERT INTO t (i, j) VALUES (1, 0), (200, 0), (300, 0) ON CONFLICT (i) DO UPDATE SET j = t.j + 1 /*__spqr__linearize_dispatch: ok */;
 
 COMMIT;
+
+-- with force auto-linearize
+BEGIN;
+SET __spqr__engine_v2 TO true;
+
+INSERT INTO t (i, j) VALUES (1, 0), (200, 0), (300, 0) ON CONFLICT (i) DO UPDATE SET j = t.j + 1 /*__spqr__linearize_dispatch: ok */;
+
+COMMIT;
