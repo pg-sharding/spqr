e5fbc03721ef54f2444bb078a19bc9e735e595bf
reshke <reshke@double.cloud>
2026-05-29 06:38:00 +0000

    Move sanity check earlier and rewrite some error messages

--- router/relay/executor.go
+++ router/relay/executor.go
@@ -999,9 +999,6 @@ func (s *QueryStateExecutorImpl) executeSliceGuts(qd *QueryDesc, topPlan plan.Pl
 		}
 
 		return nil
-
-	case *plan.CopyPlan:
-		return rerrors.ErrExecutorSyncLost
 	case *plan.ScatterPlan:
 		if q.OverwriteCC != nil {
 			overwriteCC = q.OverwriteCC
@@ -1109,11 +1106,11 @@ func (s *QueryStateExecutorImpl) executeSliceGuts(qd *QueryDesc, topPlan plan.Pl
 			if replyCl {
 				s.es.eMsg = v
 			}
-		// never expect these msgs
+		// skip
 		case *pgproto3.BindComplete:
-			// skip
+		// never expect these msgs
 		case *pgproto3.ParseComplete, *pgproto3.CloseComplete:
-			return rerrors.ErrExecutorSyncLost
+			return rerrors.ErrExecutorSyncLost.Detail("Unexpected prepared statement responce in plan deploy")
 		case *pgproto3.CommandComplete:
 			/*
 			* Safe for later reuse. For multi-slice statements
@@ -1151,6 +1148,12 @@ func (s *QueryStateExecutorImpl) executeSliceGuts(qd *QueryDesc, topPlan plan.Pl
 
 // TODO : unit tests
 func (s *QueryStateExecutorImpl) ExecuteSlice(qd *QueryDesc, topPlan plan.Plan, replyCl bool) error {
+
+	/* First thing first, do sanity check */
+	switch topPlan.(type) {
+	case *plan.CopyPlan:
+		return rerrors.ErrExecutorSyncLos.Detail("copy plan is not convertable to dispatchable plan")
+	}
 
 	if err := s.executeSliceGuts(qd, topPlan, replyCl); err != nil {
 		return err
