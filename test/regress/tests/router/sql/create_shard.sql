6773e1e54005b96ba7165ca2d78d3b54e07cf3b9
diphantxm <diphantxm@yandex-team.ru>
2026-06-11 18:14:39 +0300

    rewrite with regress testes

--- test/regress/tests/router/sql/create_shard.sql
+++ test/regress/tests/router/sql/create_shard.sql
@@ -5,27 +5,29 @@ CREATE DISTRIBUTED RELATION test_table (id) IN d;
 CREATE REFERENCE TABLE ref_table;
 DROP SHARD sh2;
 
-\c regress
-SET __spqr__engine_v2 TO on;
-CREATE TABLE ref_table(ref_id int) /* __spqr__execute_on: sh2 */;
+CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
 
+\c regress
+CREATE TABLE ref_table(ref_id int) /* __spqr__execute_on: sh2 */;
 \c spqr-console
 CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
 
 CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432");
 
 \c regress
+CREATE TABLE test_table(name text) /* __spqr__execute_on: sh2 */;
+\c spqr-console
+CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
+\c regress
+DROP TABLE test_table /* __spqr__execute_on: sh2 */;
 
-CREATE TABLE test_table(name text) /*__spqr__execute_on: sh2 */;
-
+CREATE TABLE test_table(id int, name text) /* __spqr__execute_on: sh2 */;
 \c spqr-console
 CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
 
 \c regress
-DROP TABLE test_table /*__spqr__execute_on: sh2 */;
-CREATE TABLE test_table(id int, name text) /*__spqr__execute_on: sh2 */;
+DROP TABLE test_table;
+DROP TABLE ref_table;
 
 \c spqr-console
-CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
-
 DROP DISTRIBUTION ALL CASCADE;
