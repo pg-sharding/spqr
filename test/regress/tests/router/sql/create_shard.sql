6312946866ede41c8001b4b1b02d467d0a180069
diphantxm <diphantxm@yandex-team.ru>
2026-06-11 21:01:10 +0300

    fix outptu and add force test

--- test/regress/tests/router/sql/create_shard.sql
+++ test/regress/tests/router/sql/create_shard.sql
@@ -12,8 +12,6 @@ CREATE TABLE ref_table(ref_id int) /* __spqr__execute_on: sh2 */;
 \c spqr-console
 CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
 
-CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432");
-
 \c regress
 CREATE TABLE test_table(name text) /* __spqr__execute_on: sh2 */;
 \c spqr-console
@@ -24,6 +22,9 @@ DROP TABLE test_table /* __spqr__execute_on: sh2 */;
 CREATE TABLE test_table(id int, name text) /* __spqr__execute_on: sh2 */;
 \c spqr-console
 CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432", USER "regress", DBNAME "regress");
+
+DROP SHARD sh2;
+CREATE SHARD sh2 OPTIONS (HOST "spqr_shard_2:6432", HOST "spqr_shard_2_replica:6432") FORCE;
 
 \c regress
 DROP TABLE test_table;
