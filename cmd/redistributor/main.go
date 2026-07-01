0863f6af89ae05b40a3ebbf4c172f0f3de0f68f5
Yury Frolov <yura200253@gmail.com>
2026-07-01 16:21:06 +0500

    Fixes

--- cmd/redistributor/main.go
+++ cmd/redistributor/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/pg-sharding/spqr/pkg"
@@ -30,7 +31,7 @@ var (
 	dryRun               bool
 
 	rootCmd = &cobra.Command{
-		Use:   "spqr-redistributor command args...",
+		Use:   "spqr-redistributor",
 		Short: "tools to help with key transfer",
 		CompletionOptions: cobra.CompletionOptions{
 			DisableDefaultCmd: true,
@@ -41,7 +42,7 @@ var (
 	}
 
 	generateTaskCmd = &cobra.Command{
-		Use:   "TODO",
+		Use:   "generate-task --cordinator-addr `coordinator grpc address` --etcd-addr `etcd address`... --chunk-size size --batch-size size --key-range-id id --shard-id id --max-tasks count [--dry-run]",
 		Short: "split a number of keys and redistribute them to a given shard",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if keyRangeID == "" {
@@ -62,7 +63,8 @@ var (
 				return fmt.Errorf("could not connect to QDB: %w", err)
 			}
 			c := coord.NewCoordinator(db, nil, qdb.DefaultMaxTxnSize)
-			ctx := context.Background()
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
 			keyRange, err := c.GetKeyRange(ctx, keyRangeID)
 			if err != nil {
 				return err
@@ -92,7 +94,7 @@ var (
 				return nil
 			}
 			nextBound, err := datatransfers.ResolveNextBound(ctx, keyRange, &c)
-			nextBoundBytes := (&kr.KeyRange{LowerBound: nextBound}).OutFunc(0)
+			nextBoundBytes := (&kr.KeyRange{LowerBound: nextBound, ColumnTypes: ds.ColTypes}).OutFunc(0)
 			nextBoundInt, _ := binary.Varint(nextBoundBytes)
 			curBound, _ := binary.Varint(keyRange.OutFunc(0))
 			keyRangeToRedistribute := keyRange.ID
@@ -101,6 +103,9 @@ var (
 				log.Printf("redistribute key range with bound %d\n", newBound)
 				return nil
 			}
+
+			krService := protos.NewKeyRangeServiceClient(conn)
+
 			if nextBoundInt-int64(chunkSize) > curBound {
 				buf := make([]byte, binary.MaxVarintLen64)
 				binary.PutVarint(buf, newBound)
@@ -109,22 +114,23 @@ var (
 					return err
 				}
 				log.Printf("splitting key range \"%s\" by %d\n", newKeyRangeID.String(), newBound)
-				if err := c.Split(ctx, &kr.SplitKeyRange{
-					Bound:      [][]byte{buf},
-					SourceID:   keyRange.ID,
-					KeyRangeID: newKeyRangeID.String(),
+				if _, err := krService.SplitKeyRange(ctx, &protos.SplitKeyRangeRequest{
+					NewId:    newKeyRangeID.String(),
+					SourceId: keyRange.ID,
+					Bound:    buf,
 				}); err != nil {
 					return err
 				}
 				keyRangeToRedistribute = newKeyRangeID.String()
 			}
 			log.Printf("redistributing key range \"%s\"\n", keyRangeToRedistribute)
-			krService := protos.NewKeyRangeServiceClient(conn)
 			_, err = krService.RedistributeKeyRange(ctx, &protos.RedistributeKeyRangeRequest{
 				Krid:      keyRangeToRedistribute,
 				BatchSize: int64(batchSize),
 				ShardId:   shardToID,
 				NoWait:    true,
+				Check:     true,
+				Apply:     true,
 			})
 			return err
 		},
@@ -140,6 +146,8 @@ func init() {
 	generateTaskCmd.Flags().StringVar(&keyRangeID, "key-range-id", "", "ID of the key range to transfer")
 	generateTaskCmd.Flags().IntVar(&maxRedistributeTasks, "max-tasks", 1, "maximum amount of redistribute tasks to run")
 	generateTaskCmd.Flags().BoolVar(&dryRun, "dry-run", false, "perform a dry run")
+
+	rootCmd.AddCommand(generateTaskCmd)
 }
 
 func main() {
