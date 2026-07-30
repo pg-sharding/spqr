572a3a828911f2ce3efac5aca5ab3fd4c614684c
reshke <reshke@double.cloud>
2026-07-30 22:53:45 +0500

    Update util.go: revert dummy changes (#2795)

--- yacc/console/util.go
+++ yacc/console/util.go
@@ -1,7 +1,7 @@
 package spqrparser
 
 import (
-	"fmt"
+	"errors"
 )
 
 // Tokenizer is the struct used to generate SQL
@@ -36,10 +36,7 @@ func setParseTree(yylex any, stmt []Statement) {
 func Parse(sql string) ([]Statement, error) {
 	tokenizer := NewStringTokenizer(sql)
 	if yyParse(tokenizer) != 0 {
-		if tokenizer.LastError == "" {
-			return nil, fmt.Errorf("failed to parse query: %q", sql)
-		}
-		return nil, fmt.Errorf("%s: %q", tokenizer.LastError, sql)
+		return nil, errors.New(tokenizer.LastError)
 	}
 	ast := tokenizer.ParseTree
 	return ast, nil
