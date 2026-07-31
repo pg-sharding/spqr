c038a666420ba0d52e39871b8532fc9b00040c95
reshke <reshke@double.cloud>
2026-07-31 09:10:29 +0000

    f

--- test/stress/pins.sh
+++ test/stress/pins.sh
@@ -3,7 +3,12 @@
 export CLIENTS=10
 
 function tester {
-	n=$(for i in `seq 1 100`; do if [[ i -eq 1 ]]; then echo 'SET __spqr__session_connections_pin TO on;'; else echo 'select pg_backend_pid() /*__spqr__execute_on: sh1 */;'; fi; done 2>&1 | psql "host=localhost port=6432 dbname=db1 user=user1" -t | sort | uniq -d | wc  -l)
+	n=$(for i in `seq 1 100`; do
+			if [[ i -eq 1 ]]; then
+				echo 'SET __spqr__session_connections_pin TO on;';
+			else
+				echo 'select pg_backend_pid() /*__spqr__execute_on: sh1 */;';
+			fi; done 2>&1 | psql "host=stress_router port=6432 dbname=db1 user=user1" -t | sort | uniq -d | wc  -l)
 	echo $n
 
 	if [[ n -ne 2 ]]; then 
