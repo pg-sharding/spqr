4f125e40b10dfdbfe1d7aca37e16ba19241f5d79
Yury Frolov <yura200253@gmail.com>
2026-08-03 16:02:25 +0500

    Adjust isolation tests

--- test/regress/tests/router/sql/spqrguard_kr_meta.sql
+++ test/regress/tests/router/sql/spqrguard_kr_meta.sql
@@ -15,7 +15,7 @@ SET __spqr__execute_on=sh2;
 SELECT * FROM spqr_metadata.spqr_local_key_ranges;
 
 \c spqr-console
-MOVE KEY RANGE kr1 TO sh2;
+MOVE KEY RANGE kr1 TO sh2 META ONLY;
 
 \c regress
 SET __spqr__execute_on=sh1;
