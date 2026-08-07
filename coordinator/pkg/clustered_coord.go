208833489ea4385c2846222488f664576fc54efd
Yury Frolov <yura200253@gmail.com>
2026-08-07 15:57:28 +0500

    Fix spellchecker

--- coordinator/pkg/clustered_coord.go
+++ coordinator/pkg/clustered_coord.go
@@ -1044,7 +1044,7 @@ func (qc *ClusteredCoordinator) Move(ctx context.Context, req *kr.MoveKeyRange, 
 		case qdb.MoveKeyRangePlanned:
 			if config.CoordinatorConfig().DataMoveOptimisticPIDAwait {
 				if err := datatransfers.AwaitPIDs(ctx, keyRange.ShardID, "key_range_move_"+move.MoveId); err != nil {
-					spqrlog.Zero.Error().Err(err).Msg("failed to await vxids")
+					spqrlog.Zero.Error().Err(err).Msg("failed to await virual transactions before move")
 					return spqrerror.Newf(spqrerror.SPQR_TRANSFER_ERROR, "failed to await virtual transactions to exit before move: %s", err)
 				}
 			}
