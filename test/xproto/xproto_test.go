1a6c5bae80fbdfa0ae4404a7ad9da11cba6b56d9
reshke <reshke@double.cloud>
2026-08-26 15:10:11 +0500

    Hash parameter OIDs in prepared stmt name (#2933)

--- test/xproto/xproto_test.go
+++ test/xproto/xproto_test.go
@@ -6871,3 +6871,93 @@ func TestTypesInXproto(t *testing.T) {
 	}
 	protoTestRunner(t, frontend, tt)
 }
+
+func TestTypesInXprotoParamOids(t *testing.T) {
+
+	frontend, conn, err := bootstrapConnection(t)
+	assert.NoError(t, err, "startup failed")
+
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	tt := []MessageGroup{
+		/* create named portal, describe, close, re-create, execute */
+		{
+			Request: []pgproto3.FrontendMessage{
+				&pgproto3.Close{
+					Name:       "stmt_types",
+					ObjectType: 'S',
+				},
+				&pgproto3.Parse{
+					Name:  "stmt_types",
+					Query: "INSERT INTO t_types(id, val, value) VALUES($1, $2, $3)",
+				},
+				&pgproto3.Parse{
+					Name:  "stmt_types",
+					Query: "INSERT INTO t_types(id, val, value) VALUES($1, $2, $3)",
+					ParameterOIDs: []uint32{
+						catalog.INT4OID,
+						catalog.INT4OID,
+						catalog.INT4OID,
+					},
+				},
+				&pgproto3.Parse{
+					Query: "BEGIN",
+				},
+				&pgproto3.Bind{},
+				&pgproto3.Execute{},
+				&pgproto3.Bind{
+					DestinationPortal: "",
+					PreparedStatement: "stmt_types",
+					Parameters: [][]byte{
+						{0x0, 0x0, 0x0, 0x1},
+						{0x0, 0x0, 0x0, 0x1},
+						{0x0, 0x0, 0x0, 0x1},
+					},
+					ParameterFormatCodes: []int16{xproto.FormatCodeBinary, xproto.FormatCodeBinary, xproto.FormatCodeBinary},
+				},
+				&pgproto3.Describe{
+					ObjectType: 'P',
+					Name:       "",
+				},
+				&pgproto3.Execute{
+					Portal: "",
+				},
+
+				&pgproto3.Parse{
+					Query: "ROLLBACK",
+				},
+				&pgproto3.Bind{},
+				&pgproto3.Execute{},
+				&pgproto3.Sync{},
+			},
+			Response: []pgproto3.BackendMessage{
+				&pgproto3.CloseComplete{},
+				&pgproto3.ParseComplete{},
+				&pgproto3.ParseComplete{},
+				&pgproto3.ParseComplete{},
+				&pgproto3.BindComplete{},
+				&pgproto3.CommandComplete{
+					CommandTag: []byte("BEGIN"),
+				},
+				&pgproto3.BindComplete{},
+				&pgproto3.NoData{},
+
+				&pgproto3.CommandComplete{
+					CommandTag: []byte("INSERT 0 1"),
+				},
+
+				&pgproto3.ParseComplete{},
+				&pgproto3.BindComplete{},
+				&pgproto3.CommandComplete{
+					CommandTag: []byte("ROLLBACK"),
+				},
+				&pgproto3.ReadyForQuery{
+					TxStatus: byte(txstatus.TXIDLE),
+				},
+			},
+		},
+	}
+	protoTestRunner(t, frontend, tt)
+}
