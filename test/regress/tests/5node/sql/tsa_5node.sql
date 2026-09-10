e95f56f129dfda0a83602e120e91af1964ed1f9f
reshke <reshke@double.cloud>
2026-09-10 12:13:31 +0300

    f

--- test/regress/tests/5node/sql/tsa_5node.sql
+++ test/regress/tests/5node/sql/tsa_5node.sql
@@ -39,6 +39,25 @@ SELECT __spqr__host_status('127.0.0.1:6434');
 SELECT __spqr__host_status('127.0.0.1:6435');
 SELECT __spqr__host_status('127.0.0.1:6436');
 SELECT __spqr__host_status('127.0.0.1:6437');
+-- cut the replicas off: prefer-standby must fall back to the primary
+SET __spqr__reply_notice TO true;
+SET __spqr__notice_message_format = '{shard}@{host}';
+SET __spqr__execute_on TO sh1;
+\! iptables -A INPUT -p tcp --dport 6434 -j REJECT
+\! iptables -A INPUT -p tcp --dport 6435 -j REJECT
+\! iptables -A INPUT -p tcp --dport 6436 -j REJECT
+\! iptables -A INPUT -p tcp --dport 6437 -j REJECT
+\! sleep 2
+SET __spqr__target_session_attrs TO 'prefer-standby';
+SELECT 1+2;
+\! iptables -D INPUT -p tcp --dport 6434 -j REJECT
+\! iptables -D INPUT -p tcp --dport 6435 -j REJECT
+\! iptables -D INPUT -p tcp --dport 6436 -j REJECT
+\! iptables -D INPUT -p tcp --dport 6437 -j REJECT
+RESET __spqr__target_session_attrs;
+RESET __spqr__notice_message_format;
+RESET __spqr__execute_on;
+SET __spqr__reply_notice TO false;
 -- the router writes distributed-relation metadata into the shard's spqr_metadata
 -- on CREATE TABLE; DROP TABLE does not scrub that row, so remove it explicitly
 DROP TABLE tsa_test;
