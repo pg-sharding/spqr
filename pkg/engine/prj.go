641fbc86c9a161d065057ac7752a4087aaefe96c
reshke <reshke@double.cloud>
2026-09-10 22:23:39 +0300

    Support partial column scan for virtual relations

--- pkg/engine/prj.go
+++ pkg/engine/prj.go
@@ -4,7 +4,33 @@ import (
 	"fmt"
 
 	"github.com/pg-sharding/spqr/pkg/tupleslot"
+	"github.com/pg-sharding/lyx/lyx"
 )
+
+// ExtractProjectionColumns returns column names from a target list,
+// or nil if any target is not a plain ColumnRef (e.g. `*`, expressions).
+func ExtractProjectionColumns(targetList []lyx.Node) []string {
+	columns := []string{}
+	for _, tle := range targetList {
+		var cr *lyx.ColumnRef
+		switch el := tle.(type) {
+		case *lyx.ColumnRef:
+			cr = el
+		case *lyx.ResTarget:
+			if c, ok := el.Value.(*lyx.ColumnRef); ok {
+				cr = c
+			}
+		}
+		if cr == nil || cr.ColName == "*" || cr.ColName == "" {
+			return nil
+		}
+		columns = append(columns, cr.ColName)
+	}
+	if len(columns) == 0 {
+		return nil
+	}
+	return columns
+}
 
 func Project(tts *tupleslot.TupleTableSlot, columns []string) (*tupleslot.TupleTableSlot, error) {
 	/* Do tuple projection */
