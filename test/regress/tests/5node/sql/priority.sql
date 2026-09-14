891ee917c9251c4b6becdbdd5e1fcaab7d3df217
reshke <reshke@double.cloud>
2026-09-14 20:37:15 +0300

    f

--- test/regress/tests/5node/sql/priority.sql
+++ test/regress/tests/5node/sql/priority.sql
@@ -54,20 +54,26 @@ SET __spqr__execute_on TO sh1;
 \! sleep 2
 -- prefer-standby picks the local-AZ replica first
 SELECT 1+2;
+
 -- disabling 6434 forces the route to the next standby
 RESET __spqr__execute_on;
 SELECT __spqr__console_execute('ALTER SHARD sh1 OPTIONS (SET HOST ''127.0.0.1:6434:local PRIORITY -1'')') /*__spqr__preferred_engine: v2 */;
+
 SET __spqr__execute_on TO sh1;
 SELECT 1+2;
 RESET __spqr__execute_on;
+
 SELECT __spqr__console_execute('ALTER SHARD sh1 OPTIONS (SET HOST ''127.0.0.1:6434:local'')') /*__spqr__preferred_engine: v2 */;
 SET __spqr__execute_on TO sh1;
+
 \! iptables -D INPUT -p tcp --dport 6433 -j REJECT
 \! iptables -D INPUT -p tcp --dport 6436 -j REJECT
 \! iptables -D INPUT -p tcp --dport 6437 -j REJECT
+
 RESET __spqr__target_session_attrs;
 RESET __spqr__notice_message_format;
 RESET __spqr__execute_on;
+
 SET __spqr__reply_notice TO false;
 
 DROP TABLE pr_test;
