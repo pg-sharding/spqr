9fefd79541f69e9643dd42126ecb4d3ccdb5b128
reshke <reshke@double.cloud>
2026-09-15 22:16:44 +0300

    g

--- test/regress/tests/router/sql/insert_select_pullup.sql
+++ test/regress/tests/router/sql/insert_select_pullup.sql
@@ -1,9 +1,7 @@
 \c spqr-console
-
 -- pullup sublist in simple single level select:
 -- INSERT ... SELECT with nested subselects must route by the pulled-up
 -- target list value instead of failing / misrouting
-
 CREATE DISTRIBUTION d (int);
 CREATE KEY RANGE k2 FROM 11 ROUTE TO sh2 FOR DISTRIBUTION d;
 CREATE KEY RANGE k1 FROM 0 ROUTE TO sh1 FOR DISTRIBUTION d;
@@ -25,6 +23,8 @@ SELECT id FROM t_city WHERE id = 1 ORDER BY 1;
 SELECT id FROM t_city WHERE id = 12 ORDER BY 1;
 
 DROP TABLE t_city;
+
+DELETE FROM spqr_metadata.spqr_distributed_relations /* __spqr__scatter_query: true */;
 
 /* __spqr__execute_on: sh1 */ SELECT * FROM spqr_metadata.spqr_distributed_relations;
 
